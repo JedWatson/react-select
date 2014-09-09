c0b65f5cd252747ca369ae9141e54af791a1a05e
Jed Watson <jed.watson@me.com>
2014-09-09 15:15:02 +1000

    Adding async example

--- examples/src/app.js
+++ examples/src/app.js
@@ -21,8 +21,60 @@ var SelectField = React.createClass({
 		</div>;
 	}
 });
+ 
+var RemoteSelectField = React.createClass({
+	loadOptions: function(input, callback) {
+		
+		var rtn = {
+			options: [
+				{ label: 'One', value: 'one' },
+				{ label: 'Two', value: 'two' },
+				{ label: 'Three', value: 'three' }
+			],
+			complete: true
+		};
+		
+		if (input.slice(0,1) === 'a') {
+			if (input.slice(0,2) === 'ab') {
+				rtn = {
+					options: [
+						{ label: 'AB', value: 'ab' },
+						{ label: 'ABC', value: 'abc' },
+						{ label: 'ABCD', value: 'abcd' }
+					],
+					complete: true
+				};
+			} else {
+				rtn = {
+					options: [
+						{ label: 'A', value: 'a' },
+						{ label: 'AA', value: 'aa' },
+						{ label: 'AB', value: 'ab' }
+					],
+					complete: false
+				};
+			}
+		} else if (!input.length) {
+			rtn.complete = false;
+		}
+		
+		setTimeout(function() {
+			callback(null, rtn);
+		}, 500);
+		
+	},
+	render: function() {
+		return <div>
+			<label>{this.props.label}</label>
+			<Select asyncOptions={this.loadOptions} value={this.props.value} />
+		</div>;
+	}
+});
 
 React.renderComponent(
-	<SelectField label="State:"/>,
+	<div>
+		<SelectField label="State:"/>
+		<RemoteSelectField label="Remote:"/>
+	</div>,
 	document.getElementById('example')
 );
