2dd77601c31bb09d1c7a0e1b56a4c957d8a588f7
Jed Watson <jed.watson@me.com>
2014-09-09 16:36:46 +1000

    Updating dist build

---
+++ dist/select.js
@@ -0,0 +1,379 @@
+require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"react-select":[function(require,module,exports){
+/** @jsx React.DOM */
+
+var _ = require('underscore'),
+	React = require('react');
+
+var noop = function() {};
+
+var logEvent = function(msg) {
+	console.log(msg);
+};
+
+// comment out this line to debug the control state
+logEvent = noop;
+
+var classes = function() {
+	var rtn = [];
+	for (var i = 0; i < arguments.length; i++) {
+		if ('string' === typeof arguments[i]) {
+			rtn.push(arguments[i]);
+		} else if (_.isObject(arguments[i])) {
+			_.each(arguments[i], function(val, key) {
+				if (val) {
+					rtn.push(key);
+				}
+			});
+		}
+	}
+	return rtn.join(' ') || undefined;
+}
+
+var requestId = 0;
+
+var Select = React.createClass({
+	
+	getDefaultProps: function() {
+		return {
+			autoload: true
+		};
+	},
+	
+	getInitialState: function() {
+		return {
+			value: this.props.value,
+			inputValue: '',
+			placeholder: '',
+			options: this.props.options || [],
+			focusedOption: null,
+			isFocused: false,
+			isOpen: false,
+			isLoading: false
+		};
+	},
+	
+	componentWillMount: function() {
+		
+		this._optionsCache = {};
+		this.setState(this.getStateFromValue(this.state.value));
+		
+		if (this.props.asyncOptions && this.props.autoload) {
+			this.autoloadAsyncOptions();
+		}
+		
+	},
+	
+	getStateFromValue: function(value) {
+		var selectedOption = ('string' === typeof value) ? _.findWhere(this.props.options, { value: value }) : value;
+		return selectedOption ? {
+			value: value,
+			inputValue: selectedOption.label,
+			placeholder: selectedOption.label,
+			focusedOption: selectedOption
+		} : {
+			value: '',
+			inputValue: '',
+			placeholder: this.props.placeholder || 'Select...',
+			focusedOption: null
+		};
+	},
+	
+	keyboardActions: {
+		13: 'selectFocusedOption',
+		27: 'closeOnEscape',
+		38: 'focusPreviousOption',
+		40: 'focusNextOption'
+	},
+	
+	handleKeyDown: function(event) {
+		logEvent('------');
+		logEvent(event);
+		var action = this.keyboardActions[event.keyCode];
+		if (!action) {
+			return;
+		}
+		event.preventDefault();
+		this[action].call(this);
+	},
+	
+	handleMouseDown: function() {
+		logEvent('click: control');
+		if (this.state.isOpen) {
+			this.setState({
+				isOpen: false
+			});
+			this._controlIsFocused = true;
+			this.refs.control.getDOMNode().focus();
+			clearTimeout(this.blurTimer);
+		} else {
+			this.setState({
+				isOpen: true,
+				inputValue: ''
+			});
+			if (!this._inputIsFocused) {
+				this.refs.input.getDOMNode().focus();
+			}
+		}
+	},
+	
+	handleFocus: function() {
+		if (this._controlIsFocused) return;
+		logEvent('focus: control');
+		this._controlIsFocused = true;
+		clearTimeout(this.blurTimer);
+		setTimeout(function() {
+			if (!this._inputIsFocused) {
+				this.refs.input.getDOMNode().focus();
+			}
+		}.bind(this), 0);
+		this.setState({
+			isFocused: true
+		});
+	},
+	
+	handleBlur: function(event) {
+		if (!this._controlIsFocused) return;
+		this._controlIsFocused = false;
+		clearTimeout(this.blurTimer);
+		this.blurTimer = setTimeout(function() {
+			logEvent('blur: control');
+			var blurState = this.getStateFromValue(this.state.value);
+			blurState.isFocused = false;
+			blurState.isOpen = false;
+			this.setState(blurState);
+		}.bind(this), 100);
+	},
+	
+	handleInputMouseDown: function(event) {
+		if (this._inputIsFocused) {
+			logEvent('click: input');
+			event.stopPropagation();
+		}
+	},
+	
+	handleInputFocus: function(event) {
+		logEvent('focus: input');
+		clearTimeout(this.blurTimer);
+		this._inputIsFocused = true;
+	},
+	
+	handleInputBlur: function(event) {
+		logEvent('blur: input');
+		this._inputIsFocused = false;
+	},
+	
+	handleInputChange: function(event) {
+		if (this.props.asyncOptions) {
+			this.setState({
+				isLoading: true,
+				inputValue: event.target.value
+			});
+			this.loadAsyncOptions(event.target.value, {
+				isLoading: false,
+				isOpen: true
+			});
+		} else {
+			this.setState({
+				isOpen: true,
+				inputValue: event.target.value
+			});
+		}
+	},
+	
+	autoloadAsyncOptions: function() {
+		this.loadAsyncOptions('', {}, function() {});
+	},
+	
+	loadAsyncOptions: function(input, state) {
+		
+		for (var i = 0; i <= input.length; i++) {
+			var cacheKey = input.slice(0, i);
+			if (this._optionsCache[cacheKey] && (input === cacheKey || this._optionsCache[cacheKey].complete)) {
+				this.setState(_.extend({
+					options: this._optionsCache[cacheKey].options
+				}, state));
+				return;
+			}
+		}
+		
+		var thisRequestId = this._currentRequestId = requestId++;
+		
+		this.props.asyncOptions(input, function(err, data) {
+			
+			this._optionsCache[input] = data;
+			
+			if (thisRequestId !== this._currentRequestId) {
+				return;
+			}
+			
+			this.setState(_.extend({
+				options: data.options
+			}, state));
+			
+		}.bind(this));
+		
+	},
+	
+	filterOptions: function() {
+		return _.filter(this.state.options, this.filterOption, this);
+	},
+	
+	filterOption: function(op) {
+		return (
+			!this.state.inputValue
+			|| op.value.toLowerCase().indexOf(this.state.inputValue.toLowerCase()) >= 0
+			|| op.label.toLowerCase().indexOf(this.state.inputValue.toLowerCase()) >= 0
+		);
+	},
+	
+	selectOption: function(option) {
+		this.setValue(option);
+		this.refs.control.getDOMNode().focus();
+	},
+	
+	setValue: function(option) {
+		var newState = this.getStateFromValue(option);
+		newState.isOpen = false;
+		this.setState(newState);
+	},
+	
+	selectFocusedOption: function() {
+		return this.setValue(this.state.focusedOption);
+	},
+	
+	clearValue: function(event) {
+		logEvent('clear value');
+		this.setValue(null);
+	},
+	
+	closeOnEscape: function() {
+		this.setValue(this.state.value);
+	},
+	
+	focusOption: function(op) {
+		this.setState({
+			focusedOption: op
+		});
+	},
+	
+	unfocusOption: function(op) {
+		if (this.state.focusedOption === op) {
+			this.setState({
+				focusedOption: null
+			});
+		}
+	},
+	
+	focusNextOption: function() {
+		this.focusAdjacentOption('next');
+	},
+	
+	focusPreviousOption: function() {
+		this.focusAdjacentOption('previous');
+	},
+	
+	focusAdjacentOption: function(dir) {
+		
+		if (!this.state.isOpen) {
+			this.setState({
+				isOpen: true,
+				inputValue: ''
+			});
+			return;
+		}
+		
+		var ops = this.filterOptions();
+		
+		if (!ops.length) {
+			return;
+		}
+		
+		var focusedIndex = -1;
+		
+		for (var i = 0; i < ops.length; i++) {
+			if (this.state.focusedOption === ops[i]) {
+				focusedIndex = i;
+				break;
+			}
+		}
+		
+		var focusedOption = ops[0];
+		
+		if (dir === 'next' && focusedIndex > -1 && focusedIndex < ops.length - 1) {
+			focusedOption = ops[focusedIndex + 1];
+		} else if (dir === 'previous') {
+			if (focusedIndex > 0) {
+				focusedOption = ops[focusedIndex - 1];
+			} else {
+				focusedOption = ops[ops.length - 1];
+			}
+		}
+		
+		this.setState({
+			focusedOption: focusedOption
+		});
+		
+	},
+	
+	buildMenu: function() {
+		
+		var ops = {};
+		
+		_.each(this.filterOptions(), function(op) {
+			
+			var optionClass = classes({
+				'Select-option': true,
+				'is-focused': this.state.focusedOption === op
+			});
+			
+			var mouseEnter = this.focusOption.bind(this, op),
+				mouseLeave = this.unfocusOption.bind(this, op),
+				mouseDown = this.selectOption.bind(this, op);
+			
+			ops[op.value] = React.DOM.div({
+				className: optionClass,
+				onMouseEnter: mouseEnter,
+				onMouseLeave: mouseLeave,
+				onMouseDown: mouseDown
+			}, op.label);
+			
+		}, this);
+		
+		if (_.isEmpty(ops)) {
+			ops._no_ops = React.DOM.div({className: "Select-noresults"}, "No results found");
+		}
+		
+		return ops;
+		
+	},
+	
+	render: function() {
+		
+		logEvent('render');
+		
+		var menu = this.state.isOpen ? React.DOM.div({ className: "Select-menu" }, this.buildMenu()) : null;
+		var loading = this.state.isLoading ? React.DOM.span({ className: "Select-loading" }) : null;
+		var clear = this.state.value ? React.DOM.span({ className: "Select-clear", onClick: this.clearValue, dangerouslySetInnerHTML: { __html: '&times;' } }) : null;
+		
+		var selectClass = classes('Select', {
+			'is-open': this.state.isOpen,
+			'is-focused': this.state.isFocused
+		});
+		
+		return React.DOM.div({ className: selectClass }, 
+			React.DOM.input({ type: "hidden", ref: "value", name: this.props.name, value: this.state.value }), 
+			React.DOM.div({ className: "Select-control", tabIndex: "-1", ref: "control", onKeyDown: this.handleKeyDown, onMouseDown: this.handleMouseDown, onFocus: this.handleFocus, onBlur: this.handleBlur }, 
+				React.DOM.input({ className: "Select-input", placeholder: this.state.placeholder, ref: "input", onMouseDown: this.handleInputMouseDown, value: this.state.inputValue, onFocus: this.handleInputFocus, onBlur: this.handleInputBlur, onChange: this.handleInputChange }), 
+				React.DOM.span({ className: "Select-arrow" }),
+				loading,
+				clear
+			), 
+			menu
+		);
+	}
+	
+});
+
+module.exports = Select;
+
+},{"react":undefined,"underscore":undefined}]},{},[]);
