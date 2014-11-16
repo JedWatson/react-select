604285752568cad6bdac3315b366d294cfeb6067
Jed Watson <jed.watson@me.com>
2014-11-16 23:53:01 +1100

    Cleaning up project structure

--- dist/react-select.js
+++ dist/react-select.js
@@ -1,151 +1,7 @@
-require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+!function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.Select=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function (global){
 var _ = require('underscore'),
-	React = require('react'),
-	classes = require('classnames');
-
-var Option = React.createClass({
-	
-	displayName: 'Value',
-	
-	propTypes: {
-		label: React.PropTypes.string.isRequired
-	},
-	
-	blockEvent: function(event) {
-		event.stopPropagation();
-	},
-	
-	render: function() {
-		return React.createElement("div", {className: "Select-item"}, 
-			React.createElement("span", {className: "Select-item-icon", onMouseDown: this.blockEvent, onClick: this.props.onRemove}, "×"), 
-			React.createElement("span", {className: "Select-item-label"}, this.props.label)
-		);
-	}
-	
-});
-
-module.exports = Option;
-
-},{"classnames":2,"react":undefined,"underscore":undefined}],2:[function(require,module,exports){
-function classnames() {
-	var args = arguments, classes = [];
-	for (var i = 0; i < args.length; i++) {
-		if (args[i] && 'string' === typeof args[i]) {
-			classes.push(args[i]);
-		} else if ('object' === typeof args[i]) {
-			classes = classes.concat(Object.keys(args[i]).filter(function(cls) {
-				return args[i][cls];
-			}));
-		}
-	}
-	return classes.join(' ') || undefined;
-}
-
-module.exports = classnames;
-
-},{}],3:[function(require,module,exports){
-var React = require('react');
-
-var sizerStyle = { position: 'absolute', visibility: 'hidden', height: 0, width: 0, overflow: 'scroll', whiteSpace: 'nowrap' };
-
-var AutosizeInput = React.createClass({
-	
-	displayName: 'AutosizeInput',
-
-	propTypes: {
-		value: React.PropTypes.any,                 // field value
-		defaultValue: React.PropTypes.any,          // default field value
-		onChange: React.PropTypes.func,             // onChange handler: function(newValue) {}
-		style: React.PropTypes.object,              // css styles for the outer element
-		className: React.PropTypes.string,          // className for the outer element
-		inputStyle: React.PropTypes.object,         // css styles for the input element
-		inputClassName: React.PropTypes.string      // className for the input element
-	},
-	
-	getDefaultProps: function() {
-		return {
-			minWidth: 1
-		};
-	},
-	
-	getInitialState: function() {
-		return {
-			inputWidth: this.props.minWidth
-		};
-	},
-	
-	componentDidMount: function() {
-		this.copyInputStyles();
-		this.updateInputWidth();
-	},
-	
-	componentDidUpdate: function() {
-		this.updateInputWidth();
-	},
-	
-	copyInputStyles: function() {
-		if (!this.isMounted() || !window.getComputedStyle) {
-			return;
-		}
-		var inputStyle = window.getComputedStyle(this.refs.input.getDOMNode());
-		var widthNode = this.refs.sizer.getDOMNode();
-		widthNode.style.fontSize = inputStyle.fontSize;
-		widthNode.style.fontFamily = inputStyle.fontFamily;
-	},
-	
-	updateInputWidth: function() {
-		if (!this.isMounted()) {
-			return;
-		}
-		var newInputWidth = this.refs.sizer.getDOMNode().scrollWidth + 2;
-		if (newInputWidth < this.props.minWidth) {
-			newInputWidth = this.props.minWidth;
-		}
-		if (newInputWidth !== this.state.inputWidth) {
-			this.setState({
-				inputWidth: newInputWidth
-			});
-		}
-	},
-	
-	getInput: function() {
-		return this.refs.input;
-	},
-	
-	focus: function() {
-		this.refs.input.getDOMNode().focus();
-	},
-	
-	select: function() {
-		this.refs.input.getDOMNode().select();
-	},
-	
-	render: function() {
-		
-		var nbspValue = (this.props.value || '').replace(/ /g, '&nbsp;');
-		
-		var wrapperStyle = this.props.style || {};
-		wrapperStyle.display = 'inline-block';
-		
-		var inputStyle = this.props.inputStyle || {};
-		inputStyle.width = this.state.inputWidth;
-		
-		return (
-			React.createElement("div", {className: this.props.className, style: wrapperStyle}, 
-				React.createElement("input", React.__spread({},  this.props, {ref: "input", className: this.props.inputClassName, style: inputStyle})), 
-				React.createElement("div", {ref: "sizer", style: sizerStyle, dangerouslySetInnerHTML: { __html: nbspValue}})
-			)
-		);
-		
-	}
-	
-});
-
-module.exports = AutosizeInput;
-
-},{"react":undefined}],"react-select":[function(require,module,exports){
-var _ = require('underscore'),
-	React = require('react'),
+	React = (typeof window !== "undefined" ? window.React : typeof global !== "undefined" ? global.React : null),
 	Input = require('react-input-autosize'),
 	classes = require('classnames'),
 	Value = require('./value');
@@ -201,14 +57,12 @@ var Select = React.createClass({
 	},
 	
 	componentWillMount: function() {
-		
 		this._optionsCache = {};
 		this.setState(this.getStateFromValue(this.props.value));
 		
 		if (this.props.asyncOptions && this.props.autoload) {
 			this.autoloadAsyncOptions();
 		}
-		
 	},
 	
 	componentWillUnmount: function() {
@@ -216,8 +70,16 @@ var Select = React.createClass({
 		clearTimeout(this._focusTimeout);
 	},
 	
+	componentWillReceiveProps: function(newProps) {
+		if (newProps.value !== this.state.value) {
+			this.setState(this.getStateFromValue(newProps.value));
+		}
+		if (newProps.options !== this.props.options) {
+			this.setState({ options: newProps.options });
+		}
+	},
+	
 	componentDidUpdate: function() {
-		console.log('component did update, focus: ' + this._focusAfterUpdate);
 		if (this._focusAfterUpdate) {
 			clearTimeout(this._blurTimeout);
 			this._focusTimeout = setTimeout(function() {
@@ -318,7 +180,6 @@ var Select = React.createClass({
 	
 	handleInputBlur: function(event) {
 		this._blurTimeout = setTimeout(function() {
-			console.log('blur: ' + this._focusAfterUpdate);
 			if (this._focusAfterUpdate) return;
 			this.setState({
 				isOpen: false,
@@ -580,4 +441,153 @@ var Select = React.createClass({
 
 module.exports = Select;
 
-},{"./value":1,"classnames":2,"react":undefined,"react-input-autosize":3,"underscore":undefined}]},{},[]);
+}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
+},{"./value":4,"classnames":2,"react-input-autosize":3,"underscore":undefined}],2:[function(require,module,exports){
+function classnames() {
+	var args = arguments, classes = [];
+	for (var i = 0; i < args.length; i++) {
+		if (args[i] && 'string' === typeof args[i]) {
+			classes.push(args[i]);
+		} else if ('object' === typeof args[i]) {
+			classes = classes.concat(Object.keys(args[i]).filter(function(cls) {
+				return args[i][cls];
+			}));
+		}
+	}
+	return classes.join(' ') || undefined;
+}
+
+module.exports = classnames;
+
+},{}],3:[function(require,module,exports){
+var React = require('react');
+
+var sizerStyle = { position: 'absolute', visibility: 'hidden', height: 0, width: 0, overflow: 'scroll', whiteSpace: 'nowrap' };
+
+var AutosizeInput = React.createClass({
+	
+	displayName: 'AutosizeInput',
+
+	propTypes: {
+		value: React.PropTypes.any,                 // field value
+		defaultValue: React.PropTypes.any,          // default field value
+		onChange: React.PropTypes.func,             // onChange handler: function(newValue) {}
+		style: React.PropTypes.object,              // css styles for the outer element
+		className: React.PropTypes.string,          // className for the outer element
+		inputStyle: React.PropTypes.object,         // css styles for the input element
+		inputClassName: React.PropTypes.string      // className for the input element
+	},
+	
+	getDefaultProps: function() {
+		return {
+			minWidth: 1
+		};
+	},
+	
+	getInitialState: function() {
+		return {
+			inputWidth: this.props.minWidth
+		};
+	},
+	
+	componentDidMount: function() {
+		this.copyInputStyles();
+		this.updateInputWidth();
+	},
+	
+	componentDidUpdate: function() {
+		this.updateInputWidth();
+	},
+	
+	copyInputStyles: function() {
+		if (!this.isMounted() || !window.getComputedStyle) {
+			return;
+		}
+		var inputStyle = window.getComputedStyle(this.refs.input.getDOMNode());
+		var widthNode = this.refs.sizer.getDOMNode();
+		widthNode.style.fontSize = inputStyle.fontSize;
+		widthNode.style.fontFamily = inputStyle.fontFamily;
+	},
+	
+	updateInputWidth: function() {
+		if (!this.isMounted()) {
+			return;
+		}
+		var newInputWidth = this.refs.sizer.getDOMNode().scrollWidth + 2;
+		if (newInputWidth < this.props.minWidth) {
+			newInputWidth = this.props.minWidth;
+		}
+		if (newInputWidth !== this.state.inputWidth) {
+			this.setState({
+				inputWidth: newInputWidth
+			});
+		}
+	},
+	
+	getInput: function() {
+		return this.refs.input;
+	},
+	
+	focus: function() {
+		this.refs.input.getDOMNode().focus();
+	},
+	
+	select: function() {
+		this.refs.input.getDOMNode().select();
+	},
+	
+	render: function() {
+		
+		var nbspValue = (this.props.value || '').replace(/ /g, '&nbsp;');
+		
+		var wrapperStyle = this.props.style || {};
+		wrapperStyle.display = 'inline-block';
+		
+		var inputStyle = this.props.inputStyle || {};
+		inputStyle.width = this.state.inputWidth;
+		
+		return (
+			React.createElement("div", {className: this.props.className, style: wrapperStyle}, 
+				React.createElement("input", React.__spread({},  this.props, {ref: "input", className: this.props.inputClassName, style: inputStyle})), 
+				React.createElement("div", {ref: "sizer", style: sizerStyle, dangerouslySetInnerHTML: { __html: nbspValue}})
+			)
+		);
+		
+	}
+	
+});
+
+module.exports = AutosizeInput;
+
+},{"react":undefined}],4:[function(require,module,exports){
+(function (global){
+var _ = require('underscore'),
+	React = (typeof window !== "undefined" ? window.React : typeof global !== "undefined" ? global.React : null),
+	classes = require('classnames');
+
+var Option = React.createClass({
+	
+	displayName: 'Value',
+	
+	propTypes: {
+		label: React.PropTypes.string.isRequired
+	},
+	
+	blockEvent: function(event) {
+		event.stopPropagation();
+	},
+	
+	render: function() {
+		return React.createElement("div", {className: "Select-item"}, 
+			React.createElement("span", {className: "Select-item-icon", onMouseDown: this.blockEvent, onClick: this.props.onRemove}, "×"), 
+			React.createElement("span", {className: "Select-item-label"}, this.props.label)
+		);
+	}
+	
+});
+
+module.exports = Option;
+
+}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
+},{"classnames":2,"underscore":undefined}]},{},[1])(1)
+});
