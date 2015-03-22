2ed666621450706dca608cac7947dc535557c68d
Jed Watson <jed.watson@me.com>
2015-03-22 23:32:27 +1100

    Fixing linter warnings

--- src/Value.js
+++ src/Value.js
@@ -1,43 +1,42 @@
-var _ = require('lodash'),
-	React = require('react');
+var React = require('react');
 
 var Option = React.createClass({
-	
+
 	displayName: 'Value',
-	
+
 	propTypes: {
 		label: React.PropTypes.string.isRequired
 	},
-	
+
 	blockEvent: function(event) {
-		event.stopPropagation(); 
+		event.stopPropagation();
 	},
-	
+
 	render: function() {
 		var label = this.props.label;
-		
+
 		if (this.props.optionLabelClick) {
 			label = (
 				<a className="Select-item-label__a"
-				   onMouseDown={this.blockEvent}
-				   onTouchEnd={this.props.onOptionLabelClick}
-				   onClick={this.props.onOptionLabelClick}>
+					onMouseDown={this.blockEvent}
+					onTouchEnd={this.props.onOptionLabelClick}
+					onClick={this.props.onOptionLabelClick}>
 					{label}
 				</a>
 			);
 		}
-		
+
 		return (
 			<div className="Select-item">
 				<span className="Select-item-icon"
-				      onMouseDown={this.blockEvent}
-				      onClick={this.props.onRemove}
-				      onTouchEnd={this.props.onRemove}>&times;</span>
+					onMouseDown={this.blockEvent}
+					onClick={this.props.onRemove}
+					onTouchEnd={this.props.onRemove}>&times;</span>
 				<span className="Select-item-label">{label}</span>
 			</div>
 		);
 	}
-	
+
 });
 
 module.exports = Option;
