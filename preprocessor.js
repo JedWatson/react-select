c08baa6fb6fd55d7176d4c0f81b6e50844b8f924
Matt Dell <mattdell29@gmail.com>
2015-03-25 10:32:01 +0000

    Adding Jest unit test framework
    Adding Jest unit test framework and one simple unit test to get started.

---
+++ preprocessor.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var ReactTools = require('react-tools');
+
+module.exports = {
+  process: function(src) {
+	return ReactTools.transform(src);
+  }
+};
---
+++ src/__tests__/Select-test.js
@@ -0,0 +1,35 @@
+/** @jsx React.DOM */
+'use strict';
+jest.dontMock('../Select');
+jest.dontMock('../Value');
+
+var React = require('react/addons');
+var Select = require('../Select');
+var TestUtils = React.addons.TestUtils;
+
+describe('Select test', function() {
+
+    var options = [
+        { value: 'one', label: 'One' },
+        { value: 'two', label: 'Two' }
+    ];
+
+    function logChange(val) {
+        console.log('Selected: ' + val);
+    }
+
+    // Render an instance of the component
+    var instance = TestUtils.renderIntoDocument(
+        <Select
+            name="form-field-name"
+            value="one"
+            options={options}
+            onChange={logChange}/>
+    );
+
+	var selectInputElement = TestUtils.findRenderedDOMComponentWithTag(instance, 'input');
+
+	it('should assign the given name', function() {
+		expect(selectInputElement.getDOMNode().name).toEqual('form-field-name');
+	});
+});
