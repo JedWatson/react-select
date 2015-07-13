cce032cae9e3d810d1a4dcb0565871985a1847f7
Dave Brotherstone <davegb@pobox.com>
2015-07-13 20:35:50 +0200

    Add more tests for simple keyboard actions

--- test/Select-test.js
+++ test/Select-test.js
@@ -18,16 +18,19 @@ var TestUtils = React.addons.TestUtils;
 
 var Select = require('../src/Select');
 
+
 describe('Select', function() {
 
 	describe('with simple options', function () {
 		var options, instance, onChange;
+		var searchInputNode;
 
 		beforeEach(function () {
 
 			options = [
 				{value: 'one', label: 'One'},
-				{value: 'two', label: 'Two'}
+				{value: 'two', label: 'Two'},
+				{value: 'three', label: 'Three'}
 			];
 			
 			onChange = sinon.spy();
@@ -39,15 +42,87 @@ describe('Select', function() {
 					value="one"
 					options={options}
 					onChange={onChange}
+					searchable={true}
 					/>
 			);
+			
+			// Focus on the input, such that mouse events are accepted
+			searchInputNode = instance.getInputNode().getDOMNode().querySelector('input');
+			TestUtils.Simulate.focus(searchInputNode);
 
 		});
+		
+		function pressEnterToAccept() {
+			TestUtils.Simulate.keyDown(searchInputNode, { keyCode: 13, key: 'Enter' });
+		}
+		function pressTabToAccept() {
+			TestUtils.Simulate.keyDown(searchInputNode, { keyCode: 9, key: 'Tab' });
+		}
+		
+		function typeSearchText(text) {
+			TestUtils.Simulate.change(searchInputNode, { target: { value: text } });
+		}
 
 		it('should assign the given name', function () {
 			var selectInputElement = TestUtils.scryRenderedDOMComponentsWithTag(instance, 'input')[0];
 			expect(React.findDOMNode(selectInputElement).name, 'to equal', 'form-field-name');
 		});
 		
+		it('should show the options on mouse click', function () {
+			TestUtils.Simulate.mouseDown(React.findDOMNode(instance).querySelector('.Select-control'));
+			var node = React.findDOMNode(instance);
+			expect(node, 'queried for', '.Select-option', 'to have length', 3);
+		});
+		
+		it('should display the labels on mouse click', function () {
+			TestUtils.Simulate.mouseDown(React.findDOMNode(instance).querySelector('.Select-control'));
+			var node = React.findDOMNode(instance);
+			expect(node, 'queried for', '.Select-option:nth-child(1)', 'to have items satisfying', 'to have text', 'One');
+			expect(node, 'queried for', '.Select-option:nth-child(2)', 'to have items satisfying', 'to have text', 'Two');
+			expect(node, 'queried for', '.Select-option:nth-child(3)', 'to have items satisfying', 'to have text', 'Three');
+		});
+		
+		it('should filter after entering some text', function () {
+			
+			typeSearchText('T');
+			var node = React.findDOMNode(instance);
+			expect(node, 'queried for', '.Select-option:nth-child(1)', 'to have items satisfying', 'to have text', 'Two');
+			expect(node, 'queried for', '.Select-option:nth-child(2)', 'to have items satisfying', 'to have text', 'Three');
+			expect(node, 'queried for', '.Select-option', 'to have length', 2);
+		});
+		
+		it('should filter case insensitively', function () {
+
+			typeSearchText('t');
+			var node = React.findDOMNode(instance);
+			expect(node, 'queried for', '.Select-option:nth-child(1)', 'to have items satisfying', 'to have text', 'Two');
+			expect(node, 'queried for', '.Select-option:nth-child(2)', 'to have items satisfying', 'to have text', 'Three');
+			expect(node, 'queried for', '.Select-option', 'to have length', 2);
+		});
+		
+		it('should filter using "contains"', function () {
+			
+			// Search 'h', should only show 'Three'
+			typeSearchText('h');
+			var node = React.findDOMNode(instance);
+			expect(node, 'queried for', '.Select-option:nth-child(1)', 'to have items satisfying', 'to have text', 'Three');
+			expect(node, 'queried for', '.Select-option', 'to have length', 1);
+		});
+		
+		it('should accept when enter is pressed', function () {
+
+			// Search 'h', should only show 'Three'
+			typeSearchText('h');
+			pressEnterToAccept();
+			expect(onChange, 'was called with', 'three');
+		});
+		
+		it('should accept when tab is pressed', function () {
+
+			// Search 'h', should only show 'Three'
+			typeSearchText('h');
+			pressTabToAccept();
+			expect(onChange, 'was called with', 'three');
+		});
 	});
 });
