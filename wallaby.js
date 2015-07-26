a59f61b88e4a509eb4253127536442738279fb4b
Jed Watson <jed.watson@me.com>
2015-07-27 01:24:54 +1000

    Adding explanation for wallaby.js

--- wallaby.js
+++ wallaby.js
@@ -1,17 +1,16 @@
+/*
+  This is the config file for the [Wallabyjs](http://wallabyjs.com) test runner
+*/
+
 var babel = require('babel');
 
 module.exports = function (wallaby) {
-	
 	return {
-
 		files: ['src/*.js', 'testHelpers/*.js'],
-
 		tests: ['test/*-test.js' ],
-
 		env: {
 			type: 'node'
 		},
-
 		preprocessors: {
 			'**/*.js': file => babel.transform(file.content, {sourceMap: true})
 		}
