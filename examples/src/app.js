a0834310190b1a6a4742d5f8226a46664003ae21
Dave Brotherstone <davegb@pobox.com>
2015-08-08 13:22:07 +0200

    Add examples for values as numbers
    Improves the logChange() method to basically work like console.log, with variable parameters.

--- examples/src/app.js
+++ examples/src/app.js
@@ -9,8 +9,8 @@ var STATES = require('./data/states');
 var USERS = require('./data/users');
 var id = 0;
 
-function logChange(value) {
-	console.log('Select value changed: ' + value);
+function logChange() {
+	console.log.apply(console, [].concat(['Select value changed: '], Array.prototype.slice.apply(arguments)));
 }
 
 var CountrySelect = React.createClass({
@@ -41,6 +41,95 @@ var UsersField = React.createClass({
 					optionComponent={GravatarOption}
 					singleValueComponent={GravatarValue}
 					options={USERS.users}/>
+			</div>
+		);
+	}
+});
+
+var ValuesAsNumbersField = React.createClass({
+	
+	getInitialState: function () {
+		return {
+			options: [
+				{ value: 10, label: 'Ten' },
+				{ value: 11, label: 'Eleven' },
+				{ value: 12, label: 'Twelve' },
+				{ value: 23, label: 'Twenty-three' },
+				{ value: 24, label: 'Twenty-three' }
+			],
+			matchPos: 'any',
+			matchValue: true,
+			matchLabel: true,
+			value: null,
+			multi: false
+		};
+	},
+	
+	onChangeMatchStart(event) {
+		this.setState({
+			matchPos: event.target.checked ? 'start' : 'any'
+		});
+	},
+
+	onChangeMatchValue(event) {
+		this.setState({
+			matchValue: event.target.checked
+		});
+	},
+
+	onChangeMatchLabel(event) {
+		this.setState({
+			matchLabel: event.target.checked
+		});
+	},
+	
+	onChange(value, values) {
+		this.setState({
+			value: value
+		});
+		logChange(value, values);
+	},
+	
+	onChangeMulti(event) {
+		this.setState({
+			multi: event.target.checked
+		});
+	},
+	
+	render: function () {
+		
+		var matchProp = 'any';
+		
+		if (this.state.matchLabel && !this.state.matchValue) {
+			matchProp = 'label';
+		}
+		
+		if (!this.state.matchLabel && this.state.matchValue) {
+			matchProp = 'value';
+		}
+		
+		return (
+			<div>
+				<label>{this.props.label}</label>
+				<Select
+					searchable={true}
+					matchProp={matchProp}
+					matchPos={this.state.matchPos}
+					options={this.state.options}
+					onChange={this.onChange}
+					value={this.state.value}
+					multi={this.state.multi}
+					/>
+				<div>
+					<label htmlFor="values-as-numbers-multi">Multi-Select?</label>
+					<input type="checkbox" id="values-as-numbers-multi" checked={this.state.multi} onChange={this.onChangeMulti} />
+					<label htmlFor="values-as-numbers-matchstart">Match only at start?</label>
+					<input type="checkbox" id="values-as-numbers-matchstart" checked={this.state.matchPos === 'start'} onChange={this.onChangeMatchStart} />
+					<label htmlFor="values-as-numbers-matchvalue">Match value?</label>
+					<input type="checkbox" id="values-as-numbers-matchvalue" checked={this.state.matchValue} onChange={this.onChangeMatchValue} />
+					<label htmlFor="values-as-numbers-matchlabel">Match label?</label>
+					<input type="checkbox" id="values-as-numbers-matchlabel" checked={this.state.matchLabel} onChange={this.onChangeMatchLabel} />
+				</div>
 			</div>
 		);
 	}
@@ -341,6 +430,7 @@ React.render(
 		<StatesField />
 		<StatesField label="States (non-searchable):" searchable={false} />
 		<UsersField />
+		<ValuesAsNumbersField label="Values as numbers" />
 		<MultiSelectField label="Multiselect:"/>
 		<SelectedValuesField label="Clickable labels (labels as links):" />
 		<SelectedValuesFieldDisabled label="Disabled option:" />
