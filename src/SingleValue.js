b4718d482515a3d6e665ef52c275d2f272fd8568
Jed Watson <jed.watson@me.com>
2015-08-09 14:45:00 +1000

    Fixing whitespace

--- src/SingleValue.js
+++ src/SingleValue.js
@@ -1,16 +1,15 @@
 var React = require('react');
 
 var SingleValue = React.createClass({
-  propTypes: {
-    placeholder: React.PropTypes.string,           // this is default value provided by React-Select based component
-    value: React.PropTypes.object                  // selected option
-  },
-
-  render: function() {
-    return (
-      <div className="Select-placeholder">{this.props.placeholder}</div>
-    );
-  }
+	propTypes: {
+		placeholder: React.PropTypes.string,       // this is default value provided by React-Select based component
+		value: React.PropTypes.object              // selected option
+	},
+	render: function() {
+		return (
+			<div className="Select-placeholder">{this.props.placeholder}</div>
+		);
+	}
 });
 
 module.exports = SingleValue;
