af92c2cfcab00bb5f624457e9e1711df00157bf2
Ben Stahl <bens@vimeo.com>
2015-08-14 14:03:31 -0400

    Ignore disabled option clicks unless target is a link - fixes #381

--- src/Option.js
+++ src/Option.js
@@ -12,13 +12,30 @@ var Option = React.createClass({
 		renderFunc: React.PropTypes.func               // method passed to ReactSelect component to render label text
 	},
 
+	blockEvent: function(event) {
+		event.preventDefault();
+		if ((event.target.tagName !== 'A') || !('href' in event.target)) {
+			return;
+		}
+
+		if (event.target.target) {
+			window.open(event.target.href);
+		} else {
+			window.location.href = event.target.href;
+		}
+	},
+
 	render: function() {
 		var obj = this.props.option;
 		var renderedLabel = this.props.renderFunc(obj);
 		var optionClasses = classes(this.props.className, obj.className);
 
 		return obj.disabled ? (
-			<div className={optionClasses}>{renderedLabel}</div>
+			<div className={optionClasses}
+				onMouseDown={this.blockEvent}
+				onClick={this.blockEvent}>
+				{renderedLabel}
+			</div>
 		) : (
 			<div className={optionClasses}
 				 style={obj.style}
