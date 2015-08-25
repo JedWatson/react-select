ef670697ec714dcca65e43e06ad80a637601bb68
Jed Watson <jed.watson@me.com>
2015-08-25 18:50:13 +1000

    Update 2015-08-25T08:50:05.633Z

--- bundle.js
+++ bundle.js
@@ -17,6 +17,19 @@ var Option = React.createClass({
 		renderFunc: React.PropTypes.func // method passed to ReactSelect component to render label text
 	},
 
+	blockEvent: function blockEvent(event) {
+		event.preventDefault();
+		if (event.target.tagName !== 'A' || !('href' in event.target)) {
+			return;
+		}
+
+		if (event.target.target) {
+			window.open(event.target.href);
+		} else {
+			window.location.href = event.target.href;
+		}
+	},
+
 	render: function render() {
 		var obj = this.props.option;
 		var renderedLabel = this.props.renderFunc(obj);
@@ -24,7 +37,9 @@ var Option = React.createClass({
 
 		return obj.disabled ? React.createElement(
 			'div',
-			{ className: optionClasses },
+			{ className: optionClasses,
+				onMouseDown: this.blockEvent,
+				onClick: this.blockEvent },
 			renderedLabel
 		) : React.createElement(
 			'div',
