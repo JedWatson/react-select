a39352ea6a7c2fb21c6b8b4275918d4f3a814e4a
Colby Rabideau <crabideau@hubspot.com>
2015-09-24 16:38:03 -0400

    s/space/tabs

--- examples/src/components/GroupedOptionsField.js
+++ examples/src/components/GroupedOptionsField.js
@@ -6,44 +6,44 @@ function logChange() {
 }
 
 var ops = [{
-  label: 'Black',
-  value: 'black',
+	label: 'Black',
+	value: 'black',
 }, {
-  label: 'Primary Colors',
-  options: [{
-    label: 'Yellow',
-    value: 'yellow'
-  }, {
-    label: 'Red',
-    value: 'red'
-  }, {
-    label: 'Blue',
-    value: 'blue'
-  }]
+	label: 'Primary Colors',
+	options: [{
+		label: 'Yellow',
+		value: 'yellow'
+	}, {
+		label: 'Red',
+		value: 'red'
+	}, {
+		label: 'Blue',
+		value: 'blue'
+	}]
 }, {
-  label: 'Secondary Colors',
-  options: [{
-    label: 'Orange',
-    value: 'orange'
-  }, {
-    label: 'Purple',
-    options: [{
-      label: 'Light Purple',
-      value: 'light_purple'
-    }, {
-      label: 'Medium Purple',
-      value: 'medium_purple'
-    }, {
-      label: 'Dark Purple',
-      value: 'dark_purple'
-    }]
-  }, {
-    label: 'Green',
-    value: 'green'
-  }]
+	label: 'Secondary Colors',
+	options: [{
+		label: 'Orange',
+		value: 'orange'
+	}, {
+		label: 'Purple',
+		options: [{
+			label: 'Light Purple',
+			value: 'light_purple'
+		}, {
+			label: 'Medium Purple',
+			value: 'medium_purple'
+		}, {
+			label: 'Dark Purple',
+			value: 'dark_purple'
+		}]
+	}, {
+		label: 'Green',
+		value: 'green'
+	}]
 }, {
-  label: 'White',
-  value: 'white',
+	label: 'White',
+	value: 'white',
 }];
 
 var GroupedOptionsField = React.createClass({
