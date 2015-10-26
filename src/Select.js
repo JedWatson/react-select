b81f1e0a0c45a1afa304a2ba79e5790742f24e56
Pete Nykanen <pete.a.nykanen@gmail.com>
2015-10-26 19:46:17 +0200

    IE-fix: don't blur if document.activeElement is menuDOM

--- src/Select.js
+++ src/Select.js
@@ -437,6 +437,11 @@ var Select = React.createClass({
 	},
 
 	handleInputBlur (event) {
+		var menuDOM = ReactDOM.findDOMNode(this.refs.menu);
+		if (document.activeElement.isEqualNode(menuDOM)) {
+			return;
+		}
+
 		this._blurTimeout = setTimeout(() => {
 			if (this._focusAfterUpdate || !this.isMounted()) return;
 			this.setState({
