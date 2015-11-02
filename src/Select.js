ba985f19d29edaded792ad8fe2d233cb95be52f0
Jed Watson <jed.watson@me.com>
2015-11-02 17:38:42 +1100

    Cleaning up propTypes and defaultProps

--- src/Select.js
+++ src/Select.js
@@ -14,58 +14,56 @@ var Select = React.createClass({
 	displayName: 'Select',
 
 	propTypes: {
-		addLabelText: React.PropTypes.string,      // placeholder displayed when you want to add a label on a multi-value input
-		allowCreate: React.PropTypes.bool,         // whether to allow creation of new entries
-		asyncOptions: React.PropTypes.func,        // function to call to get options
-		autoload: React.PropTypes.bool,            // whether to auto-load the default async options set
-		backspaceRemoves: React.PropTypes.bool,    // whether backspace removes an item if there is no text input
-		cacheAsyncResults: React.PropTypes.bool,   // whether to allow cache
-		className: React.PropTypes.string,         // className for the outer element
-		clearAllText: React.PropTypes.string,      // title for the "clear" control when multi: true
-		clearValueText: React.PropTypes.string,    // title for the "clear" control
-		clearable: React.PropTypes.bool,           // should it be possible to reset value
-		delimiter: React.PropTypes.string,         // delimiter to use to join multiple values
-		disabled: React.PropTypes.bool,            // whether the Select is disabled or not
-		filterOption: React.PropTypes.func,        // method to filter a single option  (option, filterString)
-		filterOptions: React.PropTypes.func,       // method to filter the options array: function ([options], filterString, [values])
-		ignoreCase: React.PropTypes.bool,          // whether to perform case-insensitive filtering
-		inputProps: React.PropTypes.object,        // custom attributes for the Input (in the Select-control) e.g: {'data-foo': 'bar'}
-		isLoading: React.PropTypes.bool,           // whether the Select is loading externally or not (such as options being loaded)
-		labelKey: React.PropTypes.string,          // path of the label value in option objects
-		matchPos: React.PropTypes.string,          // (any|start) match the start or entire string when filtering
-		matchProp: React.PropTypes.string,         // (any|label|value) which option property to filter on
-		multi: React.PropTypes.bool,               // multi-value input
-		multiValueComponent: React.PropTypes.func, // value component to render when multiple is set to true
-		name: React.PropTypes.string,              // field name, for hidden <input /> tag
-		newOptionCreator: React.PropTypes.func,    // factory to create new options when allowCreate set
-		noResultsText: React.PropTypes.string,     // placeholder displayed when there are no matching search results
-		onBlur: React.PropTypes.func,              // onBlur handler: function (event) {}
-		onChange: React.PropTypes.func,            // onChange handler: function (newValue) {}
-		onFocus: React.PropTypes.func,             // onFocus handler: function (event) {}
-		onInputChange: React.PropTypes.func,       // onInputChange handler: function (inputValue) {}
-		onOptionLabelClick: React.PropTypes.func,  // onCLick handler for value labels: function (value, event) {}
-		optionComponent: React.PropTypes.func,     // option component to render in dropdown
-		optionRenderer: React.PropTypes.func,      // optionRenderer: function (option) {}
-		options: React.PropTypes.array,            // array of options
-		placeholder: React.PropTypes.string,       // field placeholder, displayed when there's no value
-		searchable: React.PropTypes.bool,          // whether to enable searching feature or not
-		searchingText: React.PropTypes.string,     // message to display whilst options are loading via asyncOptions
-		searchPromptText: React.PropTypes.string,  // label to prompt for search input
-		singleValueComponent: React.PropTypes.func,// value component to render when multiple is set to false
-		value: React.PropTypes.any,                // initial field value
-		valueKey: React.PropTypes.string,          // path of the label value in option objects
-		valueRenderer: React.PropTypes.func        // valueRenderer: function (option) {}
+		addLabelText: React.PropTypes.string,       // placeholder displayed when you want to add a label on a multi-value input
+		allowCreate: React.PropTypes.bool,          // whether to allow creation of new entries
+		asyncOptions: React.PropTypes.func,         // function to call to get options
+		autoload: React.PropTypes.bool,             // whether to auto-load the default async options set
+		backspaceRemoves: React.PropTypes.bool,     // whether backspace removes an item if there is no text input
+		cacheAsyncResults: React.PropTypes.bool,    // whether to allow cache
+		className: React.PropTypes.string,          // className for the outer element
+		clearAllText: React.PropTypes.string,       // title for the "clear" control when multi: true
+		clearValueText: React.PropTypes.string,     // title for the "clear" control
+		clearable: React.PropTypes.bool,            // should it be possible to reset value
+		delimiter: React.PropTypes.string,          // delimiter to use to join multiple values
+		disabled: React.PropTypes.bool,             // whether the Select is disabled or not
+		filterOption: React.PropTypes.func,         // method to filter a single option  (option, filterString)
+		filterOptions: React.PropTypes.func,        // method to filter the options array: function ([options], filterString, [values])
+		ignoreCase: React.PropTypes.bool,           // whether to perform case-insensitive filtering
+		inputProps: React.PropTypes.object,         // custom attributes for the Input (in the Select-control) e.g: {'data-foo': 'bar'}
+		isLoading: React.PropTypes.bool,            // whether the Select is loading externally or not (such as options being loaded)
+		labelKey: React.PropTypes.string,           // path of the label value in option objects
+		matchPos: React.PropTypes.string,           // (any|start) match the start or entire string when filtering
+		matchProp: React.PropTypes.string,          // (any|label|value) which option property to filter on
+		multi: React.PropTypes.bool,                // multi-value input
+		multiValueComponent: React.PropTypes.func,  // value component to render when multiple is set to true
+		name: React.PropTypes.string,               // field name, for hidden <input /> tag
+		newOptionCreator: React.PropTypes.func,     // factory to create new options when allowCreate set
+		noResultsText: React.PropTypes.string,      // placeholder displayed when there are no matching search results
+		onBlur: React.PropTypes.func,               // onBlur handler: function (event) {}
+		onChange: React.PropTypes.func,             // onChange handler: function (newValue) {}
+		onFocus: React.PropTypes.func,              // onFocus handler: function (event) {}
+		onInputChange: React.PropTypes.func,        // onInputChange handler: function (inputValue) {}
+		onOptionLabelClick: React.PropTypes.func,   // onCLick handler for value labels: function (value, event) {}
+		optionComponent: React.PropTypes.func,      // option component to render in dropdown
+		optionRenderer: React.PropTypes.func,       // optionRenderer: function (option) {}
+		options: React.PropTypes.array,             // array of options
+		placeholder: React.PropTypes.string,        // field placeholder, displayed when there's no value
+		searchable: React.PropTypes.bool,           // whether to enable searching feature or not
+		searchingText: React.PropTypes.string,      // message to display whilst options are loading via asyncOptions
+		searchPromptText: React.PropTypes.string,   // label to prompt for search input
+		singleValueComponent: React.PropTypes.func, // value component to render when multiple is set to false
+		value: React.PropTypes.any,                 // initial field value
+		valueKey: React.PropTypes.string,           // path of the label value in option objects
+		valueRenderer: React.PropTypes.func         // valueRenderer: function (option) {}
 	},
 
 	getDefaultProps () {
 		return {
 			addLabelText: 'Add "{label}"?',
 			allowCreate: false,
-			asyncOptions: undefined,
 			autoload: true,
 			backspaceRemoves: true,
 			cacheAsyncResults: true,
-			className: undefined,
 			clearAllText: 'Clear all',
 			clearValueText: 'Clear value',
 			clearable: true,
@@ -77,21 +75,15 @@ var Select = React.createClass({
 			labelKey: 'label',
 			matchPos: 'any',
 			matchProp: 'any',
+			multi: false,
 			multiValueComponent: MultiValue,
-			name: undefined,
-			newOptionCreator: undefined,
 			noResultsText: 'No results found',
-			onChange: undefined,
-			onInputChange: undefined,
-			onOptionLabelClick: undefined,
 			optionComponent: Option,
-			options: undefined,
 			placeholder: 'Select...',
 			searchable: true,
 			searchingText: 'Searching...',
 			searchPromptText: 'Type to search',
 			singleValueComponent: SingleValue,
-			value: undefined,
 			valueKey: 'value',
 		};
 	},
