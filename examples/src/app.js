1e4de671f651eec0e25a22593079b937bf25f939
Jed Watson <jed.watson@me.com>
2015-11-05 01:37:29 +1100

    Improving custom render example

--- examples/src/app.js
+++ examples/src/app.js
@@ -6,27 +6,12 @@ import Select from 'react-select';
 
 import Contributors from './components/Contributors';
 import CustomComponents from './components/CustomComponents';
-import CustomRenderField from './components/CustomRenderField';
-import DisabledUpsellOptions from './components/DisabledUpsellOptions';
+import CustomRender from './components/CustomRender';
 import Multiselect from './components/Multiselect';
 import NumericSelect from './components/NumericSelect';
 import RemoteSelectField from './components/RemoteSelectField';
 import SelectedValuesField from './components/SelectedValuesField';
 import States from './components/States';
-
-var FLAVOURS = [
-	{ label: 'Chocolate', value: 'chocolate' },
-	{ label: 'Vanilla', value: 'vanilla' },
-	{ label: 'Strawberry', value: 'strawberry' },
-	{ label: 'Cookies and Cream', value: 'cookiescream' },
-	{ label: 'Peppermint', value: 'peppermint' }
-];
-var FLAVOURS_WITH_DISABLED_OPTION = FLAVOURS.slice(0);
-FLAVOURS_WITH_DISABLED_OPTION.unshift({ label: 'Caramel (You don\'t like it, apparently)', value: 'caramel', disabled: true });
-
-function logChange() {
-	console.log.apply(console, [].concat(['Select value changed:'], Array.prototype.slice.apply(arguments)));
-}
 
 ReactDOM.render(
 	<div>
@@ -34,13 +19,10 @@ ReactDOM.render(
 		<Multiselect label="Multiselect" />
 		<Contributors label="Contributors" />
 		<NumericSelect label="Numeric Values" />
+		<CustomRender label="Custom Render Methods"/>
 		<CustomComponents label="Custom Option and Value Components" />
 		{/*
-		<SelectedValuesField label="Clickable labels (labels as links)" options={FLAVOURS} hint="Open the console to see click behaviour (data/event)" />
-		<SelectedValuesField label="Disabled option" options={FLAVOURS_WITH_DISABLED_OPTION} hint="You savage! Caramel is the best..." />
-		<DisabledUpsellOptions label="Disabled option with a link"/>
 		<SelectedValuesField label="Option Creation (tags mode)" options={FLAVOURS} allowCreate hint="Enter a value that's NOT in the list, then hit return" />
-		<CustomRenderField label="Custom render options/values" />
 		<RemoteSelectField label="Remote Options" hint='Type anything in the remote example to asynchronously load options. Valid alternative results are "A", "AA", and "AB"' />
 		*/}
 	</div>,
--- examples/src/components/CustomRender.js
+++ examples/src/components/CustomRender.js
@@ -1,39 +1,45 @@
 import React from 'react';
 import Select from 'react-select';
 
-function logChange() {
-	console.log.apply(console, [].concat(['Select value changed:'], Array.prototype.slice.apply(arguments)));
-}
-
 var DisabledUpsellOptions = React.createClass({
 	displayName: 'DisabledUpsellOptions',
 	propTypes: {
 		label: React.PropTypes.string,
 	},
-	onLabelClick: function (data, event) {
-		console.log(data, event);
+	getInitialState () {
+		return {};
+	},
+	setValue (value) {
+		this.setState({ value });
+		console.log('Support level selected:', value.label);
 	},
 	renderLink: function() {
 		return <a style={{ marginLeft: 5 }} href="/upgrade" target="_blank">Upgrade here!</a>;
 	},
 	renderOption: function(option) {
-		return <span>{option.label} {option.link} </span>;
+		return <span style={{ color: option.color }}>{option.label} {option.link}</span>;
+	},
+	renderValue: function(option) {
+		return <strong style={{ color: option.color }}>{option.label}</strong>;
 	},
 	render: function() {
-		var ops = [
-			{ label: 'Basic customer support', value: 'basic' },
-			{ label: 'Premium customer support', value: 'premium' },
+		var options = [
+			{ label: 'Basic customer support', value: 'basic', color: '#E31864' },
+			{ label: 'Premium customer support', value: 'premium', color: '#6216A3' },
 			{ label: 'Pro customer support', value: 'pro', disabled: true, link: this.renderLink() },
 		];
 		return (
 			<div className="section">
 				<h3 className="section-heading">{this.props.label}</h3>
 				<Select
-					onOptionLabelClick={this.onLabelClick}
 					placeholder="Select your support level"
-					options={ops}
+					options={options}
 					optionRenderer={this.renderOption}
-					onChange={logChange} />
+					onChange={this.setValue}
+					value={this.state.value}
+					valueRenderer={this.renderValue}
+					/>
+				<div className="hint">This demonstates custom render methods and links in disabled options</div>
 			</div>
 		);
 	}
