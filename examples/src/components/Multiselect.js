d534706a2a561febc45f7023eda778e2107bc690
Jed Watson <jed.watson@me.com>
2015-11-05 00:59:01 +1100

    Adding disabled option example to the multiselect example

--- examples/src/components/Multiselect.js
+++ examples/src/components/Multiselect.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import Select from 'react-select';
 
-function logChange() {
-	console.log.apply(console, [].concat(['Select value changed:'], Array.prototype.slice.apply(arguments)));
-}
+const FLAVOURS = [
+	{ label: 'Chocolate', value: 'chocolate' },
+	{ label: 'Vanilla', value: 'vanilla' },
+	{ label: 'Strawberry', value: 'strawberry' },
+	{ label: 'Caramel', value: 'caramel' },
+	{ label: 'Cookies and Cream', value: 'cookiescream' },
+	{ label: 'Peppermint', value: 'peppermint' },
+];
+
+const WHY_WOULD_YOU = [
+	{ label: 'Chocolate (are you crazy?)', value: 'chocolate', disabled: true },
+].concat(FLAVOURS.slice(1));
 
 var MultiSelectField = React.createClass({
 	displayName: 'MultiSelectField',
@@ -13,34 +22,39 @@ var MultiSelectField = React.createClass({
 	getInitialState () {
 		return {
 			disabled: false,
-			value: []
+			crazy: false,
+			options: FLAVOURS,
+			value: [],
 		};
 	},
 	handleSelectChange (value) {
-		logChange('New value:', value);
-		this.setState({ value: value });
+		console.log('You\'ve selected:', value);
+		this.setState({ value });
 	},
 	toggleDisabled (e) {
-		this.setState({ 'disabled': e.target.checked });
+		this.setState({ disabled: e.target.checked });
+	},
+	toggleChocolate (e) {
+		let crazy = e.target.checked;
+		this.setState({
+			crazy: crazy,
+			options: crazy ? WHY_WOULD_YOU : FLAVOURS,
+		});
 	},
 	render () {
-		var ops = [
-			{ label: 'Chocolate', value: 'chocolate' },
-			{ label: 'Vanilla', value: 'vanilla' },
-			{ label: 'Strawberry', value: 'strawberry' },
-			{ label: 'Caramel', value: 'caramel' },
-			{ label: 'Cookies and Cream', value: 'cookiescream' },
-			{ label: 'Peppermint', value: 'peppermint' }
-		];
 		return (
 			<div className="section">
 				<h3 className="section-heading">{this.props.label}</h3>
-				<Select multi simpleValue disabled={this.state.disabled} value={this.state.value} placeholder="Select your favourite(s)" options={ops} onChange={this.handleSelectChange} />
+				<Select multi simpleValue disabled={this.state.disabled} value={this.state.value} placeholder="Select your favourite(s)" options={this.state.options} onChange={this.handleSelectChange} />
 
 				<div className="checkbox-list">
 					<label className="checkbox">
 						<input type="checkbox" className="checkbox-control" checked={this.state.disabled} onChange={this.toggleDisabled} />
-						<span className="checkbox-label">Disabled</span>
+						<span className="checkbox-label">Disable the control</span>
+					</label>
+					<label className="checkbox">
+						<input type="checkbox" className="checkbox-control" checked={this.state.crazy} onChange={this.toggleChocolate} />
+						<span className="checkbox-label">I don't like Chocolate (disabled the option)</span>
 					</label>
 				</div>
 			</div>
