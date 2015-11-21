b34bfaa94aa4f32ddfb43c553060a38385d39dba
albert.oclarit <bbjao143213620>
2015-11-21 22:18:26 +0800

    Clicking on scrollbar will hide the options (a bug reappear?). this.handleMouseDownOnMenu might be an oversite.

--- src/Select.js
+++ src/Select.js
@@ -644,7 +644,7 @@ const Select = React.createClass({
 				</div>
 				{isOpen ? (
 					<div ref="menuContainer" className="Select-menu-outer" style={this.props.menuContainerStyle}>
-						<div ref="menu" className="Select-menu" style={this.props.menuStyle} onScroll={this.handleMenuScroll} onMouseDown={this.handleMouseDownOnMenu}>
+						<div ref="menu" className="Select-menu" style={this.props.menuStyle} onScroll={this.handleMenuScroll} onMouseDown={this.handleMouseDown}>
 							{this.renderMenu(options, !this.props.multi ? valueArray : null, focusedOption)}
 						</div>
 					</div>
