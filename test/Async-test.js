5f6b7b6b1e92aa48b632488ba2cecd226387f771
Dave Brotherstone <davegb@pobox.com>
2015-11-28 13:11:34 +0100

    Emulating DOM even for shallow renderer tests
    The DOM is still needed for the other tests, and `npm test` may run the async tests
    first.  This means React is already required, and has already decided that there is no
    DOM, hence `TestUtils.renderIntoDocument` won't work. Adding the DOM emulation
    to Async-test fixes this problem.

--- test/Async-test.js
+++ test/Async-test.js
@@ -1,3 +1,9 @@
+// Emulating the DOM here, only so that if this test file gets
+// included first, then React thinks there's a DOM, so the other tests
+// (e.g. Select-test.js) that do require a DOM work correctly
+
+var jsdomHelper = require('../testHelpers/jsdomHelper');
+jsdomHelper();
 var unexpected = require('unexpected');
 var unexpectedReact = require('unexpected-react');
 var unexpectedSinon = require('unexpected-sinon');
@@ -13,9 +19,6 @@ var sinon = require('sinon');
 
 
 var Select = require('../src/Select');
-if (!global.document) {
-	global.document = {};
-}
 
 describe('Async', () => {
 
