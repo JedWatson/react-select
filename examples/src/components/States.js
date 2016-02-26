145116cd58a62e788bd28889fe5dd35f811c2ef5
Martin Jujou <mjujou@gmail.com>
2016-02-27 01:20:19 +1100

    Fixed bug with getFocusableOption where it would not return the correct focused option

--- examples/src/components/States.js
+++ examples/src/components/States.js
@@ -47,7 +47,19 @@ var StatesField = React.createClass({
 		this.setState(newState);
 	},
 	render () {
-		var options = STATES[this.state.country];
+		// var options = STATES[this.state.country];
+
+		var options = [
+    { value: 'australian-capital-territory', label: 'Australian Capital Territory', className: 'State-ACT' },
+    { value: 'new-south-wales', label: 'New South Wales', className: 'State-NSW' },
+    { value: 'victoria', label: 'Victoria', className: 'State-Vic' },
+    { value: 'queensland', label: 'Queensland', className: 'State-Qld' },
+    { value: 'western-australia', label: 'Western Australia', className: 'State-WA' },
+    { value: 'south-australia', label: 'South Australia', className: 'State-SA' },
+    { value: 'tasmania', label: 'Tasmania', className: 'State-Tas' },
+    { value: 'northern-territory', label: 'Northern Territory', className: 'State-NT' },
+];
+
 		return (
 			<div className="section">
 				<h3 className="section-heading">{this.props.label}</h3>
