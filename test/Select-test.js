11b40dcbeab660f599c49e0dd9e00937795d70bf
Chua Wei Kuan <khankuan@gmail.com>
2016-04-06 17:43:09 -0700

    Does not render outer when it does not contain anything

--- test/Select-test.js
+++ test/Select-test.js
@@ -2224,6 +2224,17 @@ describe('Select', () => {
 					'to contain no elements matching', '.Select-noresults');
 			});
 
+			it('doesn\'t displays outer when menu is null', () => {
+
+				wrapper.setPropsForChild({
+					noResultsText: ''
+				});
+
+				typeSearchText('DOES NOT EXIST');
+				expect(ReactDOM.findDOMNode(instance),
+					'to contain no elements matching', '.Select-menu-outer');
+			});
+
 			it('supports updating the text', () => {
 
 				wrapper.setPropsForChild({
