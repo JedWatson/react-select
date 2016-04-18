d67698ea7a370f5aafb12006b520c99587514663
Danny Herran <danny@webfactory.ie>
2016-04-18 19:14:30 +0100

    .is-selected class bugfix for 'value' objects that don't have exactly the same structure/order as 'options'

--- src/Select.js
+++ src/Select.js
@@ -31,6 +31,29 @@ function stringifyValue (value) {
 	}
 }
 
+if (!Array.prototype.findIndex) {
+  Array.prototype.findIndex = function(predicate) {
+    if (this === null) {
+      throw new TypeError('Array.prototype.findIndex called on null or undefined');
+    }
+    if (typeof predicate !== 'function') {
+      throw new TypeError('predicate must be a function');
+    }
+    var list = Object(this);
+    var length = list.length >>> 0;
+    var thisArg = arguments[1];
+    var value;
+
+    for (var i = 0; i < length; i++) {
+      value = list[i];
+      if (predicate.call(thisArg, value, i, list)) {
+        return i;
+      }
+    }
+    return -1;
+  };
+}
+
 const stringOrNode = PropTypes.oneOfType([
 	PropTypes.string,
 	PropTypes.node
