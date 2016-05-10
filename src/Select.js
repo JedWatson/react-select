94effbb301903a6d3ecdaf57025e624981e953c8
Andrew Selbie <aselbie@gmail.com>
2016-05-09 18:06:27 -0700

    Always pass valueArray to renderOuter

--- src/Select.js
+++ src/Select.js
@@ -891,7 +891,7 @@ const Select = React.createClass({
 					{this.renderClear()}
 					{this.renderArrow()}
 				</div>
-				{isOpen ? this.renderOuter(options, !this.props.multi ? valueArray : null, focusedOption) : null}
+				{isOpen ? this.renderOuter(options, valueArray, focusedOption) : null}
 			</div>
 		);
 	}
