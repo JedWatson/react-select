e5ae4c04506b6497613c3c0e1f728068a9c6fd12
Sara Tansey <ucsf-pivotal@googlegroups.com>
2016-07-13 10:14:21 -0700

    Remove minWidth prop from autosized inputs. See issue #1063.
    In version 15.2.1 of React, the adding a minWidth prop to an input produces a warning like this:
    
    warning.js:44 Warning: Unknown prop `minWidth` on <input> tag. Remove this prop from the element. For details, see https://fb.me/react-unknown-prop
        in input (created by AutosizeInput)
        in div (created by AutosizeInput)
        in AutosizeInput (created by Select)
        in div (created by Select)
        in div (created by Select)
        in Select (created by PatientSelectInput)

--- src/Select.js
+++ src/Select.js
@@ -784,7 +784,7 @@ const Select = React.createClass({
 
 			if (this.props.autosize) {
 				return (
-					<Input {...inputProps} minWidth="5px" />
+					<Input {...inputProps} />
 				);
 			}
 			return (
