17b4efe075c911b46f759a5b5ff23dc3fc0a23d9
Daniel Heath <daniel@heath.cc>
2016-10-04 13:15:23 +1100

    Clear Async options after making a selection

--- src/Async.js
+++ src/Async.js
@@ -8,7 +8,7 @@ const propTypes = {
 	children: React.PropTypes.func.isRequired,       // Child function responsible for creating the inner Select component; (props: Object): PropTypes.element
 	ignoreAccents: React.PropTypes.bool,             // strip diacritics when filtering; defaults to true
 	ignoreCase: React.PropTypes.bool,                // perform case-insensitive filtering; defaults to true
-	loadingPlaceholder: React.PropTypes.oneOfType([  // replaces the placeholder while options are loading 
+	loadingPlaceholder: React.PropTypes.oneOfType([  // replaces the placeholder while options are loading
 		React.PropTypes.string,
 		React.PropTypes.node
 	]),
@@ -64,6 +64,10 @@ export default class Async extends Component {
 				});
 			}
 		});
+	}
+
+	clearOptions() {
+		this.setState({options: []});
 	}
 
 	loadOptions (inputValue) {
@@ -142,7 +146,13 @@ export default class Async extends Component {
 			noResultsText: isLoading ? loadingPlaceholder : searchPromptText,
 			placeholder: isLoading ? loadingPlaceholder : placeholder,
 			options: isLoading ? [] : options,
-			ref: (ref) => (this.select = ref)
+			ref: (ref) => (this.select = ref),
+			onChange: (newValues) => {
+				if (newValues.length > this.props.value.length) {
+					this.clearOptions();
+				}
+				this.props.onChange(newValues);
+			}
 		};
 
 		return children({
