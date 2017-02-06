9cd8ea7a72eeff13956f610ed4f619fe48c11178
Will Dembinski <willdembinski@gmail.com>
2017-02-06 12:20:28 -0800

    Added a new option to ignore punctuation, i think this could be better but will wait for comments

---
+++ test/stripPunctuation-test.js
@@ -0,0 +1,28 @@
+'use strict';
+/* global describe, it, beforeEach */
+const expect = require('chai').expect;
+const stripPunctuation = require('../lib/utils/stripPunctuation');
+
+describe('Value component', function() {
+    it('it should removed punctuation from strings', function() {
+        var cases = [
+            'Im a little teatpot.',
+            'Im, a little teatpot',
+            'Im/ a little teatpot',
+            'Im a little# teatpot',
+            'Im a little% teatpot',
+            'Im a little teatpot^',
+            'Im a little teatpot;',
+            'Im a little teatpot*',
+            'Im a little teatpot:',
+            '{Im a little teatpot}',
+            'Im a little= teatpot',
+            'Im -a -little -teatpot',
+            'Im ~a ~little ~teatpot',
+            '(Im a little teatpot)',
+            '`Im a little teatpot`'
+        ];
+        var expected = 'Im a little teatpot';
+        cases.forEach( tcase => expect(stripPunctuation(tcase)).to.eql(expected) );
+    });
+});
