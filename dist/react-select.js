a2eff0db68a265f93aee9ca65fc87b9e0d077f8f
Max Hubenthal <212883@carmax.com>
2017-02-15 14:08:54 -0500

    Revert "Build JS"
    This reverts commit 21438ed5cb109529fd8367120cf77f46e1840190.
    
    Undo changes to master

--- dist/react-select.js
+++ dist/react-select.js
@@ -1173,7 +1173,7 @@ var Select = _react2['default'].createClass({
 			});
 		} else {
 			// otherwise, focus the input and open the menu
-			this._openAfterFocus = this.props.openOnFocus;
+			this._openAfterFocus = true;
 			this.focus();
 		}
 	},
@@ -1733,7 +1733,7 @@ var Select = _react2['default'].createClass({
 			return _react2['default'].createElement('div', _extends({}, divProps, {
 				role: 'combobox',
 				'aria-expanded': isOpen,
-				'aria-owns': isOpen ? this._instancePrefix + '-list' : '',
+				'aria-owns': isOpen ? this._instancePrefix + '-list' : this._instancePrefix + '-value',
 				'aria-activedescendant': isOpen ? this._instancePrefix + '-option-' + focusedOptionIndex : this._instancePrefix + '-value',
 				className: className,
 				tabIndex: this.props.tabIndex || 0,
