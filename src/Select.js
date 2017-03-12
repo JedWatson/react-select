1b06702ef4a15be0ab495995f8ca4a020bbb845f
Edmond Pruteanu <edmond@swiftshift.com>
2017-03-12 02:56:17 +0200

    Fixed Numeric Values Multi-Select
    Selected options weren’t being added to the input in Multi-Select mode
    for Numeric Values

--- src/Select.js
+++ src/Select.js
@@ -568,7 +568,13 @@ const Select = React.createClass({
 		/** support optionally passing in the `nextProps` so `componentWillReceiveProps` updates will function as expected */
 		const props = typeof nextProps === 'object' ? nextProps : this.props;
 		if (props.multi) {
-			if (typeof value === 'string') value = value.split(props.delimiter);
+			if (typeof value === 'string') {
+				value = value.split(props.delimiter);
+				// convert string array to numbers array if made of numbers only
+				if (!value.some(isNaN)) {
+					value = value.map(Number);
+				}				
+			}
 			if (!Array.isArray(value)) {
 				if (value === null || value === undefined) return [];
 				value = [value];
