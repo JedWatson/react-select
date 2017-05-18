8392f5b40387d207beab3fbce8450608dc221aa0
Thomas Ladd <thomasladd@ip-192-168-7-70.ec2.internal>
2017-05-18 13:28:32 -0500

    1.0.3

--- dist/@helpdotcom/react-select.js
+++ dist/@helpdotcom/react-select.js
@@ -1457,7 +1457,9 @@ var Select = _react2['default'].createClass({
 				inputValue: '',
 				focusedIndex: null
 			}, function () {
-				_this3.props.onInputChange('');
+				if (_this3.props.onInputChange) {
+					_this3.props.onInputChange('');
+				}
 				_this3.addValue(value);
 			});
 		} else {
@@ -1466,7 +1468,9 @@ var Select = _react2['default'].createClass({
 				inputValue: '',
 				isPseudoFocused: this.state.isFocused
 			}, function () {
-				_this3.props.onInputChange('');
+				if (_this3.props.onInputChange) {
+					_this3.props.onInputChange('');
+				}
 				_this3.setValue(value);
 			});
 		}
