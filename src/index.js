648580a166ec22d08ea6a2360ca922a29a1554c7
Adam Girton <adam.girton@me.com>
2017-07-04 20:25:10 -0700

    Make components static fields on Select instance

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import Async from './Async';
 import AsyncCreatable from './AsyncCreatable';
 import Creatable from './Creatable';
 
+Select.Async = Async;
+Select.AsyncCreatable = AsyncCreatable;
+Select.Creatable = Creatable;
+
 export default Select;
 export {
 	Async,
