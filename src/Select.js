e4f5bdc1a23d9cf3beafc7c7d4597bce7bdd1dcd
Kiril Nedelev <kiril@collegeswimming.com>
2017-08-17 16:58:36 +0300

    add inputDisabled option

--- src/Select.js
+++ src/Select.js
@@ -766,7 +766,8 @@ class Select extends React.Component {
 			onFocus: this.handleInputFocus,
 			ref: ref => this.input = ref,
 			required: this.state.required,
-			value: this.state.inputValue
+			value: this.state.inputValue,
+			disabled: this.props.inputDisabled,
 		});
 
 		if (this.props.inputRenderer) {
@@ -990,6 +991,7 @@ class Select extends React.Component {
 			'Select--single': !this.props.multi,
 			'is-clearable': this.props.clearable,
 			'is-disabled': this.props.disabled,
+			'is-input-disabled': this.props.inputDisabled,
 			'is-focused': this.state.isFocused,
 			'is-loading': this.props.isLoading,
 			'is-open': isOpen,
@@ -1065,6 +1067,7 @@ Select.propTypes = {
     filterOptions: PropTypes.any,         // boolean to enable default filtering or function to filter the options array ([options], filterString, [values])
     ignoreAccents: PropTypes.bool,        // whether to strip diacritics when filtering
     ignoreCase: PropTypes.bool,           // whether to perform case-insensitive filtering
+    inputDisabled: PropTypes.bool,        // whether the input is disabled or not
     inputProps: PropTypes.object,         // custom attributes for the Input
     inputRenderer: PropTypes.func,        // returns a custom input component
     instanceId: PropTypes.string,         // set the components instanceId
@@ -1127,6 +1130,7 @@ Select.defaultProps = {
     deleteRemoves: true,
     delimiter: ',',
     disabled: false,
+    inputDisabled: false,
     escapeClearsValue: true,
     filterOptions: defaultFilterOptions,
     ignoreAccents: true,
