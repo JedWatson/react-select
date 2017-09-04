7a45539babfbab065af2b75b380fd606c4c845f3
Prof Gra <christophegragnic@gmail.com>
2017-09-04 22:17:56 +0200

    Export the Value component

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import Select from './Select';
 import Async from './Async';
 import AsyncCreatable from './AsyncCreatable';
 import Creatable from './Creatable';
+import Value from './Value';
 
 Select.Async = Async;
 Select.AsyncCreatable = AsyncCreatable;
 Select.Creatable = Creatable;
+Select.Value = Value;
 
 export default Select;
 export {
 	Async,
 	AsyncCreatable,
-	Creatable
+	Creatable,
+	Value
 };
