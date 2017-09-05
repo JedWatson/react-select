fa81280eff9a41eed4b50d919d2656597f65234c
Charles Lee <cc.lee@live.com.au>
2017-09-05 15:01:16 +1000

    coveraged configuration fixed, contrb document edited

--- src/index.umd.js
+++ src/index.umd.js
@@ -1,3 +1,6 @@
+// This file exists as an entry point for bundling our umd builds.
+// Both in rollup and in webpack, umd builds built from es6 modules are not compatible with mixed imports. (which exist in index.js)
+// This file does away with named imports in favor of a single export default.
 import Select from './Select';
 import Async from './Async';
 import AsyncCreatable from './AsyncCreatable';
