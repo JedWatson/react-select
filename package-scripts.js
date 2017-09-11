25ec5724a98b9313bf32c699cc769ecbd3429ee8
Jed Watson <jed.watson@me.com>
2017-09-12 00:25:56 +1000

    Add es6 build and clean up config, fixes #1981

--- package-scripts.js
+++ package-scripts.js
@@ -5,35 +5,31 @@ const rimraf = npsUtils.rimraf;
 const concurrent = npsUtils.concurrent;
 
 module.exports = {
-  scripts: {
-    build: {
-      description: 'clean dist directory and run all builds',
-      default: series(
-        rimraf('dist'),
-        rimraf('lib'),
-        concurrent.nps('build.css', 'build.min'),
-        concurrent.nps('build.umd', 'build.umdMin', 'build.es', 'build.standalone')
-      ),
-      umd: 'rollup --config',
-      umdMin: 'rollup --config --environment MINIFY',
-      es: 'babel src -d lib',
-      css: 'lessc less/default.less dist/react-select.css',
-      min: 'lessc --clean-css less/default.less dist/react-select.min.css',
-      standalone: series(
-        'cp examples/src/standalone.html examples/dist/standalone.html',
-        'lessc examples/src/example.less examples/dist/example.less'
-      ),
-      examples: series(
-        'webpack --progress -p',
-        'cp examples/src/.gitignore examples/dist/.gitignore',
-        'git subtree push --prefix examples/dist origin gh-pages'
-      )
-    },
-    publish: {
-      default: series(
-        rimraf('examples/dist'),
-        concurrent.nps('build.examples')
-      )
-    }
-  },
+	scripts: {
+		build: {
+			description: 'clean dist directory and run all builds',
+			default: series(
+				rimraf('dist'),
+				rimraf('lib'),
+				concurrent.nps('build.css', 'build.cssmin'),
+				concurrent.nps('build.rollup', 'build.babel')
+			),
+			rollup: 'rollup --config',
+			babel: 'babel src -d lib',
+			css: 'lessc less/default.less dist/react-select.css',
+			cssmin: 'lessc --clean-css less/default.less dist/react-select.min.css',
+			standalone: series(
+				'cp examples/src/standalone.html examples/dist/standalone.html',
+				'lessc examples/src/example.less examples/dist/example.css'
+			),
+		},
+		publish: {
+			default: series(
+				rimraf('examples/dist'),
+				'webpack --progress -p',
+				'cp examples/src/.gitignore examples/dist/.gitignore',
+				'git subtree push --prefix examples/dist origin gh-pages'
+			),
+		},
+	},
 };
--- rollup.config.js
+++ rollup.config.js
@@ -1,38 +1,53 @@
-
 import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
 import uglify from 'rollup-plugin-uglify';
 import { minify } from 'uglify-es';
-const min = process.env.MINIFY;
 
-export default {
-    input: 'src/index.umd.js',
-    output: {
-      name: 'Select',
-      file: min ? 'dist/react-select.min.js' : 'dist/react-select.js',
-      format: 'umd',
-    },
-    globals: {
-      react: 'React',
-      'prop-types': 'PropTypes',
-      'react-dom': 'ReactDOM',
-      'react-input-autosize': 'AutosizeInput',
-      'classnames': 'classNames',
-    },
-    external: [
-      'classnames',
-      'prop-types',
-      'react-input-autosize',
-      'react',
-      'react-dom',
-    ],
-    plugins: [
-      babel({
-        babelrc: false,
-        presets: [['es2015', { 'modules': false }], 'stage-0', 'react'],
-        plugins: ['external-helpers']
-      }),
-      resolve(),
-      min ? uglify({}, minify) : {},
-    ]
+const name = 'Select';
+const path = 'dist/react-select';
+const globals = {
+	classnames: 'classNames',
+	'prop-types': 'PropTypes',
+	'react-dom': 'ReactDOM',
+	'react-input-autosize': 'AutosizeInput',
+	react: 'React',
 };
+const external = Object.keys(globals);
+const babelOptions = {
+	babelrc: false,
+	presets: [['es2015', { modules: false }], 'stage-0', 'react'],
+	plugins: ['external-helpers'],
+};
+
+export default [
+	{
+		input: 'src/index.js',
+		output: {
+			file: path + '.es.js',
+			format: 'es',
+		},
+		plugins: [babel(babelOptions)],
+	},
+	{
+		input: 'src/index.umd.js',
+		output: {
+			name: name,
+			file: path + '.js',
+			format: 'umd',
+		},
+		globals: globals,
+		external: external,
+		plugins: [babel(babelOptions), resolve()],
+	},
+	{
+		input: 'src/index.umd.js',
+		output: {
+			name: name,
+			file: path + '.min.js',
+			format: 'umd',
+		},
+		globals: globals,
+		external: external,
+		plugins: [babel(babelOptions), resolve(), uglify({}, minify)],
+	},
+];
