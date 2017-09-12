d4d836eba9f3db13595012b74a6e2f0911262ed6
Jed Watson <jed.watson@me.com>
2017-09-12 21:35:46 +1000

    Updating index.umd to export Value, ref #1962

--- src/index.umd.js
+++ src/index.umd.js
@@ -1,13 +1,17 @@
 // This file exists as an entry point for bundling our umd builds.
-// Both in rollup and in webpack, umd builds built from es6 modules are not compatible with mixed imports. (which exist in index.js)
+// Both in rollup and in webpack, umd builds built from es6 modules are not
+// compatible with mixed imports (which exist in index.js)
 // This file does away with named imports in favor of a single export default.
+
 import Select from './Select';
 import Async from './Async';
 import AsyncCreatable from './AsyncCreatable';
 import Creatable from './Creatable';
+import Value from './Value';
 
 Select.Async = Async;
 Select.AsyncCreatable = AsyncCreatable;
 Select.Creatable = Creatable;
+Select.Value = Value;
 
 export default Select;
