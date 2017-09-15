430bf2cfb9a0273484c0601751dfae0c12b591e4
Nick Ostrovsky <nick@firedev.com>
2017-09-15 10:41:13 +0700

    removeSelected: bool
    When `removeSelected` is set to `false` you can add same item multiple
    times
    
    Changed removal logic to remove values based on position

--- src/Select.js
+++ src/Select.js
@@ -560,9 +560,10 @@ class Select extends React.Component {
 		this.setValue(this.props.multi ? valueArray.slice(0, valueArray.length - 1) : null);
 	}
 
-	removeValue (value) {
+	removeValue (index) {
 		var valueArray = this.getValueArray(this.props.value);
-		this.setValue(valueArray.filter(i => i !== value));
+		valueArray.splice(index,1);
+		this.setValue(valueArray);
 		this.focus();
 	}
 
@@ -719,7 +720,7 @@ class Select extends React.Component {
 						disabled={this.props.disabled || value.clearableValue === false}
 						key={`value-${i}-${value[this.props.valueKey]}`}
 						onClick={onClick}
-						onRemove={this.removeValue}
+						onRemove={() => this.removeValue(i)}
 						value={value}
 					>
 						{renderLabel(value, i)}
@@ -979,7 +980,7 @@ class Select extends React.Component {
 
 	render () {
 		let valueArray = this.getValueArray(this.props.value);
-		let options = this._visibleOptions = this.filterOptions(this.props.multi ? this.getValueArray(this.props.value) : null);
+		let options = this._visibleOptions = this.filterOptions(this.props.multi && this.props.removeSelected ? valueArray : null);
 		let isOpen = this.state.isOpen;
 		if (this.props.multi && !options.length && valueArray.length && !this.state.inputValue) isOpen = false;
 		const focusedOptionIndex = this.getFocusableOptionIndex(valueArray[0]);
@@ -1107,6 +1108,7 @@ Select.propTypes = {
 	options: PropTypes.array,             // array of options
 	pageSize: PropTypes.number,           // number of entries to page when using page up/down keys
 	placeholder: stringOrNode,            // field placeholder, displayed when there's no value
+	removeSelected: PropTypes.bool,       // whether the selected option is removed from the dropdown on multi selects
 	required: PropTypes.bool,             // applies HTML5 required attribute when needed
 	resetValue: PropTypes.any,            // value to use when you clear the control
 	scrollMenuIntoView: PropTypes.bool,   // boolean to enable the viewport to shift so that the full menu fully visible when engaged
@@ -1157,6 +1159,7 @@ Select.defaultProps = {
 	optionComponent: Option,
 	pageSize: 5,
 	placeholder: 'Select...',
+	removeSelected: true,
 	required: false,
 	scrollMenuIntoView: true,
 	searchable: true,
