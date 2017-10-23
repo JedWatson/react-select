342136ad24ac7008a49e708c3ee178ca01bdfddc
Charles Lee <cc.lee@live.com.au>
2017-10-23 14:39:40 +1100

    replace trim fn loop with regex

--- src/utils/trim.js
+++ src/utils/trim.js
@@ -1,10 +1,3 @@
 module.exports = function trim(str) {
-    str = str.replace(/^\s+/, '');
-    for (var i = str.length - 1; i >= 0; i--) {
-        if (/\S/.test(str.charAt(i))) {
-            str = str.substring(0, i + 1);
-            break;
-        }
-    }
-    return str;
+    return str.replace(/^\s+|\s+$/g, '');
 };
