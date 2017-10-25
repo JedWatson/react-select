e6b8101f1c5a8052b14725ebbc2e5abbc4c869b0
Erkelens, Jan Paul <Janpaul.Erkelens@capitalone.com>
2017-10-25 12:33:33 -0400

    Exports Option Component

--- src/index.js
+++ src/index.js
@@ -3,16 +3,19 @@ import Async from './Async';
 import AsyncCreatable from './AsyncCreatable';
 import Creatable from './Creatable';
 import Value from './Value';
+import Option from './Option';
 
 Select.Async = Async;
 Select.AsyncCreatable = AsyncCreatable;
 Select.Creatable = Creatable;
 Select.Value = Value;
+Select.Option = Option;
 
 export default Select;
 export {
 	Async,
 	AsyncCreatable,
 	Creatable,
-	Value
+	Value,
+	Option
 };
