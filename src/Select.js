d297918bd46e7230c07366bc77015af1f3d28b1f
forum-is <forum@forum-is.de>
2017-11-10 10:48:32 +0100

    New property "preferEarlyMatch"
    The new property "preferEarlyMatch" enables to select firstly the
    option(s) which start(s) with input value

--- src/Select.js
+++ src/Select.js
@@ -895,6 +895,7 @@ class Select extends React.Component {
 					labelKey: this.props.labelKey,
 					matchPos: this.props.matchPos,
 					matchProp: this.props.matchProp,
+					preferEarlyMatch: this.props.preferEarlyMatch,
 					valueKey: this.props.valueKey,
 					trimFilter: this.props.trimFilter
 				}
@@ -1139,6 +1140,7 @@ Select.propTypes = {
 	options: PropTypes.array,             // array of options
 	pageSize: PropTypes.number,           // number of entries to page when using page up/down keys
 	placeholder: stringOrNode,            // field placeholder, displayed when there's no value
+	preferEarlyMatch: PropTypes.bool,     // boolean to enable selecting firstly the option(s) which start(s) with text input
 	removeSelected: PropTypes.bool,       // whether the selected option is removed from the dropdown on multi selects
 	required: PropTypes.bool,             // applies HTML5 required attribute when needed
 	resetValue: PropTypes.any,            // value to use when you clear the control
@@ -1191,6 +1193,7 @@ Select.defaultProps = {
 	optionComponent: Option,
 	pageSize: 5,
 	placeholder: 'Select...',
+	preferEarlyMatch: false,
 	removeSelected: true,
 	required: false,
 	rtl: false,
--- src/utils/defaultFilterOptions.js
+++ src/utils/defaultFilterOptions.js
@@ -1,7 +1,42 @@
 import stripDiacritics from './stripDiacritics';
 import trim from './trim';
 
+function renderOptions(option, filterValue, excludeOptions, props, earlyMatchRequired) {
+	if (excludeOptions && excludeOptions.indexOf(option[props.valueKey]) > -1) return false;
+	if (props.filterOption) return props.filterOption.call(this, option, filterValue);
+	if (!filterValue) return true;
+	var valueTest = String(option[props.valueKey]);
+	var labelTest = String(option[props.labelKey]);
+	if (props.ignoreAccents) {
+		if (props.matchProp !== 'label') valueTest = stripDiacritics(valueTest);
+		if (props.matchProp !== 'value') labelTest = stripDiacritics(labelTest);
+	}
+	if (props.ignoreCase) {
+		if (props.matchProp !== 'label') valueTest = valueTest.toLowerCase();
+		if (props.matchProp !== 'value') labelTest = labelTest.toLowerCase();
+	}
+	if(earlyMatchRequired) {
+		return props.matchPos === 'start' ? (
+			(props.matchProp !== 'label' && valueTest.substr(0, filterValue.length) === filterValue) ||
+			(props.matchProp !== 'value' && labelTest.substr(0, filterValue.length) === filterValue)
+		) : (
+			(props.matchProp !== 'label' && valueTest.indexOf(filterValue) == 0) ||
+			(props.matchProp !== 'value' && labelTest.indexOf(filterValue) == 0)
+		);
+	} else {
+		return props.matchPos === 'start' ? (
+			(props.matchProp !== 'label' && valueTest.substr(0, filterValue.length) === filterValue) ||
+			(props.matchProp !== 'value' && labelTest.substr(0, filterValue.length) === filterValue)
+		) : (
+			(props.matchProp !== 'label' && valueTest.indexOf(filterValue) >= 0) ||
+			(props.matchProp !== 'value' && labelTest.indexOf(filterValue) >= 0)
+		);
+	}
+}
+
 function filterOptions (options, filterValue, excludeOptions, props) {
+	var _this = this;
+	
 	if (props.ignoreAccents) {
 		filterValue = stripDiacritics(filterValue);
 	}
@@ -15,31 +50,16 @@ function filterOptions (options, filterValue, excludeOptions, props) {
 	}
 
 	if (excludeOptions) excludeOptions = excludeOptions.map(i => i[props.valueKey]);
-
-	return options.filter(option => {
-		if (excludeOptions && excludeOptions.indexOf(option[props.valueKey]) > -1) return false;
-		if (props.filterOption) return props.filterOption.call(this, option, filterValue);
-		if (!filterValue) return true;
-		var valueTest = String(option[props.valueKey]);
-		var labelTest = String(option[props.labelKey]);
-
-		if (props.ignoreAccents) {
-			if (props.matchProp !== 'label') valueTest = stripDiacritics(valueTest);
-			if (props.matchProp !== 'value') labelTest = stripDiacritics(labelTest);
-		}
-
-		if (props.ignoreCase) {
-			if (props.matchProp !== 'label') valueTest = valueTest.toLowerCase();
-			if (props.matchProp !== 'value') labelTest = labelTest.toLowerCase();
-		}
-		return props.matchPos === 'start' ? (
-			(props.matchProp !== 'label' && valueTest.substr(0, filterValue.length) === filterValue) ||
-			(props.matchProp !== 'value' && labelTest.substr(0, filterValue.length) === filterValue)
-		) : (
-			(props.matchProp !== 'label' && valueTest.indexOf(filterValue) >= 0) ||
-			(props.matchProp !== 'value' && labelTest.indexOf(filterValue) >= 0)
-		);
+	var optionsStartWithFilterValue = options.filter(function (option) {
+		return renderOptions(option, filterValue, excludeOptions, props, true)
 	});
+	var optionsContainFilterValue = options.filter(function (option) {
+		return renderOptions(option, filterValue, excludeOptions, props);
+	});
+	return props.preferEarlyMatch ?
+		optionsStartWithFilterValue.concat(optionsContainFilterValue)
+		:
+		optionsContainFilterValue;
 }
 
 export default filterOptions;
