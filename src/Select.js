JedWatson/react-select

commit f6920deefd935d366cddff14cb45b7f6f950c08b

handleMouseDownOnArrow fix when openOnClick prop is set to false

src/Select.js
@@ -267,13 +267,17 @@ class Select extends React.Component {
 		}
 		// If the menu isn't open, let the event bubble to the main handleMouseDown
 		if (!this.state.isOpen) {
-			return;
+			this.setState({
+				isOpen: true,
+			});
 		}
 		// prevent default event handlers
 		event.stopPropagation();
 		event.preventDefault();
 		// close the menu
-		this.closeMenu();
+		if(this.state.isOpen){
+			this.closeMenu();
+		}
 	}
 
 	handleMouseDownOnMenu (event) {

examples/src/components/States.js
@@ -64,6 +64,7 @@ var StatesField = createClass({
 					value={this.state.selectValue}
 					onChange={this.updateValue}
 					rtl={this.state.rtl}
+					openOnClick={false}
 					searchable={this.state.searchable}
 				/>
 				<button style={{ marginTop: '15px' }} type="button" onClick={this.focusStateSelect}>Focus Select</button>

commit 262685aa7ad957bd8c59530b3a4e344ae40ac6e7

Fix an issue introduced by commit f6920de that results in multiple dropdowns being open at the same time because stopping propagation of the arrow mouse down event in select 2 when opening its dropdown does not allow the blur event to happen for select 1, so the dropdown for select 1 remains open.

src/Select.js
@@ -285,18 +285,19 @@ class Select extends React.Component {
 		if (this.props.disabled || (event.type === 'mousedown' && event.button !== 0)) {
 			return;
 		}
-		// If the menu isn't open, let the event bubble to the main handleMouseDown
-		if (!this.state.isOpen) {
+
+		if (this.state.isOpen) {
+			// prevent default event handlers
+			event.stopPropagation();
+			event.preventDefault();
+			// close the menu
+			this.closeMenu();
+		}
+		else {
+			// If the menu isn't open, let the event bubble to the main handleMouseDown
 			this.setState({
 				isOpen: true,
 			});
-		}
-		// prevent default event handlers
-		event.stopPropagation();
-		event.preventDefault();
-		// close the menu
-		if(this.state.isOpen){
-			this.closeMenu();
 		}
 	}
 