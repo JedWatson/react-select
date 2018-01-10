24e02a403fc3d33e5d12301b76a46ea9af773f88
Jed Watson <jed.watson@me.com>
2018-01-10 20:14:32 +1100

    Updating babel, rollup and flow config

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,5 @@
+// @flow
+
 import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
 import uglify from 'rollup-plugin-uglify';
@@ -6,55 +8,60 @@ import { minify } from 'uglify-es';
 const name = 'Select';
 const path = 'dist/react-select';
 const globals = {
-	classnames: 'classNames',
-	'prop-types': 'PropTypes',
-	'react-dom': 'ReactDOM',
-	'react-input-autosize': 'AutosizeInput',
-	react: 'React',
+  classnames: 'classNames',
+  glam: 'glam',
+  'prop-types': 'PropTypes',
+  'react-dom': 'ReactDOM',
+  'react-input-autosize': 'AutosizeInput',
+  react: 'React',
 };
 const external = Object.keys(globals);
-const babelOptions = (production) => {
-	let result = {
-		babelrc: false,
-		presets: [['env', { modules: false }], 'stage-0', 'react'],
-		plugins: ['external-helpers'],
-	};
-	if (production) {
-		result.plugins.push('transform-react-remove-prop-types');
-	};
-	return result;
+const babelOptions = prod => {
+  let result = {
+    babelrc: false,
+    presets: [['env', { modules: false }], 'react'],
+    plugins: [
+      'transform-class-properties',
+      'transform-object-rest-spread',
+      'external-helpers',
+    ],
+  };
+  if (prod) {
+    result.plugins.push('transform-react-remove-prop-types');
+  }
+  return result;
 };
 
 export default [
-	{
-		input: 'src/index.js',
-		output: {
-			file: path + '.es.js',
-			format: 'es',
-		},
-		external: external,
-		plugins: [babel(babelOptions(false))],
-	},
-	{
-		input: 'src/index.umd.js',
-		output: {
-			name: name,
-			file: path + '.js',
-			format: 'umd',
-		},
-		globals: globals,
-		external: external,
-		plugins: [babel(babelOptions(false)), resolve()],
-	},
-	{
-		input: 'src/index.umd.js',
-		output: {
-			name: name,
-			file: path + '.min.js',
-			format: 'umd',
-		},
-		globals: globals,
-		external: external,
-		plugins: [babel(babelOptions(true)), resolve(), uglify({}, minify)],
-	},
+  {
+    input: 'src/index.js',
+    output: {
+      file: path + '.es.js',
+      format: 'es',
+    },
+    external: external,
+    plugins: [babel(babelOptions(false))],
+  },
+  {
+    input: 'src/index.umd.js',
+    output: {
+      name: name,
+      file: path + '.js',
+      format: 'umd',
+      globals: globals,
+    },
+    external: external,
+    plugins: [babel(babelOptions(false)), resolve()],
+  },
+  {
+    input: 'src/index.umd.js',
+    output: {
+      name: name,
+      file: path + '.min.js',
+      format: 'umd',
+      globals: globals,
+    },
+    external: external,
+    plugins: [babel(babelOptions(true)), resolve(), uglify({}, minify)],
+  },
 ];
