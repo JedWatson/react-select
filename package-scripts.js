62148b248a2cb0fa1c53e3445961ca7942b323a4
Jed Watson <jed.watson@me.com>
2018-01-10 20:14:32 +1100

    Updating babel, rollup and flow config

--- package-scripts.js
+++ package-scripts.js
@@ -1,3 +1,5 @@
+// @flow
+
 const npsUtils = require('nps-utils');
 const path = require('path');
 const series = npsUtils.series;
@@ -5,24 +7,29 @@ const rimraf = npsUtils.rimraf;
 const concurrent = npsUtils.concurrent;
 
 module.exports = {
-	scripts: {
-		build: {
-			description: 'clean dist directory and run all builds',
-			default: series(
-				rimraf('dist'),
-				rimraf('lib'),
-				concurrent.nps('build.rollup', 'build.babel')
-			),
-			rollup: 'rollup --config',
-			babel: 'babel src -d lib',
-		},
-		publish: {
-			default: series(
-				rimraf('examples/dist'),
-				'webpack --progress -p',
-				'cp examples/src/.gitignore examples/dist/.gitignore',
-				'gh-pages -d examples/dist'
-			),
-		},
-	},
+  scripts: {
+    build: {
+      description: 'clean dist directory and run all builds',
+      default: series(
+        rimraf('dist'),
+        rimraf('lib'),
+        concurrent.nps('build.rollup', 'build.babel')
+      ),
+      rollup: 'rollup --config',
+      babel: 'babel src -d lib',
+      examples: series(
+        rimraf('examples/dist'),
+        'webpack --progress -p',
+        'cp examples/favicon.ico examples/dist/favicon.ico',
+        'cp examples/index.css examples/dist/index.css'
+      ),
+    },
+    publish: {
+      default: series(
+        'nps build.examples',
+        'gh-pages -d examples/dist',
+        rimraf('examples/dist')
+      ),
+    },
+  },
 };
