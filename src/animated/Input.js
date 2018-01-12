15b759bad4544716d2ad62dfae0f108078400b47
Joss Mackison <joss.mackison@gmail.com>
2018-01-12 15:31:24 +1100

    typo with import

--- src/animated/Input.js
+++ src/animated/Input.js
@@ -1,7 +1,7 @@
 // @flow
 
 import React from 'react';
-import { components } from 'react-select';
+import { components } from '../components';
 
 import { type BaseTransition } from './transitions';
 
