f4a9e7ffc994f4e808fab186e9c608ce92c3c56d
Joss Mackison <joss.mackison@gmail.com>
2018-01-18 13:14:41 +1100

    state arg is optional
    also added return value docs

--- src/utils.js
+++ src/utils.js
@@ -10,9 +10,11 @@ type List = Array<string>;
 
  Expects an array of strings OR a string/object pair:
  - className(['comp', 'comp-arg', 'comp-arg-2'])
+   @returns 'react-select__comp react-select__comp-arg react-select__comp-arg-2'
  - className('comp', { some: true, state: false })
+   @returns 'react-select__comp react-select__comp--some'
 */
-export function className(name: string | List, state: State): string {
+export function className(name: string | List, state?: State): string {
   const arr: List = Array.isArray(name) ? name : [name];
 
   // loop through state object, remove falsey values and combine with name
