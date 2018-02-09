7bf5cdafb3b8c5848d597e1901e409a3ef5fd156
Joss Mackison <joss.mackison@gmail.com>
2018-02-09 15:52:52 +1100

    export stateless component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 // @flow
-import Select from './StateManager';
 
-export default Select;
+import SelectStateful from './StateManager';
+
+export default SelectStateful;
+export { default as SelectStateless } from './Select';
 export { createFilter } from './filters';
 export { components } from './components/index';
