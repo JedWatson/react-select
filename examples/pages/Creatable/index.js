f41b4295ca8c82093ffb9c7afb3b4489a60e999b
Jed Watson <jed.watson@me.com>
2018-02-20 01:59:32 +1100

    Adding MultiTextInput example

--- examples/pages/Creatable/index.js
+++ examples/pages/Creatable/index.js
@@ -6,12 +6,13 @@ import ExampleWrapper from '../../ExampleWrapper';
 import CreatableAdvanced from './CreatableAdvanced';
 import CreatableMulti from './CreatableMulti';
 import CreatableSingle from './CreatableSingle';
+import MultiTextInput from './MultiTextInput';
 
 export default class App extends Component<*> {
   render() {
     return (
       <div>
-        <H1>Creatable (tags) Variant</H1>
+        <H1>Creatable Variant</H1>
         <p>Use the Creatable component to create new options</p>
         <p>
           <Code>{"import { Creatable } from 'react-select'"}</Code>
@@ -45,6 +46,17 @@ export default class App extends Component<*> {
           To simulate waiting for a back-end service to create a new option, the
           input is disabled for a second before the new option is added to the
           list and the value is updated.
+        </Note>
+
+        <ExampleWrapper
+          label="Multi-select text input"
+          urlPath="/examples/pages/Creatable/MultiTextInput.js"
+        >
+          <MultiTextInput />
+        </ExampleWrapper>
+        <Note>
+          This example applies several customisations to produce a text input
+          that has no menu but allows multiple values to be entered.
         </Note>
       </div>
     );
