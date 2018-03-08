7488c43fe2b01b44e53dfd219ab0b6487b667828
Charles Lee <cc.lee@live.com.au>
2018-03-09 10:47:41 +1100

    moved docs from home to async and creatable

--- docs/pages/async/index.js
+++ docs/pages/async/index.js
@@ -1,11 +1,39 @@
 // @flow
 
+import React from 'react';
+import ExampleWrapper from '../../ExampleWrapper';
 import md from '../../markdown/renderer';
+import {
+  AsyncCallbacks,
+  AsyncPromises,
+} from '../../examples';
+
 
 export default function Async() {
   return md`
     # Async
+    Use the Async component to load options from a remote source as the user types.
 
-    Content coming soon...
+    ~~~jsx
+    import Async from 'react-select/lib/Async';
+    ~~~
+
+    ${(
+      <ExampleWrapper
+        label="Callbacks"
+        urlPath="docs/home/examples/AsyncCallbacks.js"
+      >
+        <AsyncCallbacks />
+      </ExampleWrapper>
+    )}
+
+    ${(
+      <ExampleWrapper
+        label="Promises"
+        urlPath="docs/home/examples/AsyncPromises.js"
+      >
+        <AsyncPromises />
+      </ExampleWrapper>
+    )}
   `;
 }
