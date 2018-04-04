adf5061e54c39eca510887fbd6e5396d39cc92a7
ram <rambabu.1990@gmail.com>
2018-04-04 23:57:10 +0530

    Exposed promptText as a prop in Select.Creatable, default value set to 'Create option'

--- dist/react-select.es.js
+++ dist/react-select.es.js
@@ -2339,7 +2339,7 @@ var CreatableSelect = function (_React$Component) {
 				});
 
 				if (_isOptionUnique2) {
-					var prompt = promptTextCreator(this.inputValue);
+					var prompt = promptTextCreator(this.inputValue, this.props.promptText);
 
 					this._createPlaceholderOption = _newOptionCreator({
 						label: prompt,
@@ -2509,8 +2509,8 @@ var newOptionCreator = function newOptionCreator(_ref5) {
 	return option;
 };
 
-var promptTextCreator = function promptTextCreator(label) {
-	return 'Create option "' + label + '"';
+var promptTextCreator = function promptTextCreator(label, promptText) {
+	return promptText + ' "' + label + '"';
 };
 
 var shouldKeyDownEventCreateNewOption = function shouldKeyDownEventCreateNewOption(_ref6) {
@@ -2541,7 +2541,8 @@ CreatableSelect.defaultProps = {
 	menuRenderer: menuRenderer,
 	newOptionCreator: newOptionCreator,
 	promptTextCreator: promptTextCreator,
-	shouldKeyDownEventCreateNewOption: shouldKeyDownEventCreateNewOption
+	shouldKeyDownEventCreateNewOption: shouldKeyDownEventCreateNewOption,
+	promptText: 'Create option'
 };
 
 CreatableSelect.propTypes = {
@@ -2588,7 +2589,10 @@ CreatableSelect.propTypes = {
 	ref: PropTypes.func,
 
 	// Decides if a keyDown event (eg its `keyCode`) should result in the creation of a new option.
-	shouldKeyDownEventCreateNewOption: PropTypes.func
+	shouldKeyDownEventCreateNewOption: PropTypes.func,
+
+	// Prompt text which shows while creating a new option, default text is set to "Create option"
+	promptText: PropTypes.string
 };
 
 var AsyncCreatableSelect = function (_React$Component) {
