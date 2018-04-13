f519c72b9bb880793c145ac706524dc78df51456
Jed Watson <jed.watson@me.com>
2018-04-14 01:21:36 +1000

    Updating tests and snapshots for new tabIndex and classNames behaviour

--- src/__tests__/AsyncCreatable.test.js
+++ src/__tests__/AsyncCreatable.test.js
@@ -14,28 +14,45 @@ test('defaults - snapshot', () => {
 });
 
 test('creates an inner Select', () => {
-  const asyncCreatableWrapper = mount(<AsyncCreatable />);
+  const asyncCreatableWrapper = mount(
+    <AsyncCreatable className="react-select" />
+  );
   expect(asyncCreatableWrapper.find(Select).exists()).toBeTruthy();
 });
 
 test('render decorated select with props passed', () => {
-  const asyncCreatableWrapper = mount(<AsyncCreatable className="foo"/>);
+  const asyncCreatableWrapper = mount(<AsyncCreatable className="foo" />);
   expect(asyncCreatableWrapper.find(Select).props().className).toBe('foo');
 });
 
 test('to show the create option in menu', () => {
-  let asyncCreatableWrapper = mount(<AsyncCreatable/>);
-  let inputValueWrapper = asyncCreatableWrapper.find('div.react-select__input input');
+  let asyncCreatableWrapper = mount(
+    <AsyncCreatable className="react-select" />
+  );
+  let inputValueWrapper = asyncCreatableWrapper.find(
+    'div.react-select__input input'
+  );
   asyncCreatableWrapper.setProps({ inputValue: 'a' });
   inputValueWrapper.simulate('change', { currentTarget: { value: 'a' } });
-  expect(asyncCreatableWrapper.find(Option).last().text()).toBe('Create "a"');
+  expect(
+    asyncCreatableWrapper
+      .find(Option)
+      .last()
+      .text()
+  ).toBe('Create "a"');
 });
 
 test('to show loading and then create option in menu', () => {
   jest.useFakeTimers();
-  let loadOptionsSpy = jest.fn((inputValue, callback) => setTimeout(() => callback(OPTIONS), 200));
-  let asyncCreatableWrapper = mount(<AsyncCreatable loadOptions={loadOptionsSpy}/>);
-  let inputValueWrapper = asyncCreatableWrapper.find('div.react-select__input input');
+  let loadOptionsSpy = jest.fn((inputValue, callback) =>
+    setTimeout(() => callback(OPTIONS), 200)
+  );
+  let asyncCreatableWrapper = mount(
+    <AsyncCreatable className="react-select" loadOptions={loadOptionsSpy} />
+  );
+  let inputValueWrapper = asyncCreatableWrapper.find(
+    'div.react-select__input input'
+  );
   asyncCreatableWrapper.setProps({ inputValue: 'a' });
   inputValueWrapper.simulate('change', { currentTarget: { value: 'a' } });
 
@@ -45,5 +62,10 @@ test('to show loading and then create option in menu', () => {
   asyncCreatableWrapper.update();
 
   // show create options once options are loaded
-  expect(asyncCreatableWrapper.find(Option).last().text()).toBe('Create "a"');
+  expect(
+    asyncCreatableWrapper
+      .find(Option)
+      .last()
+      .text()
+  ).toBe('Create "a"');
 });
