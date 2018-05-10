d5452287ec9c5c5dc329898bd2a06c8cfc0b7678
Jed Watson <jed.watson@me.com>
2018-05-10 12:48:16 +1000

    Removing console.log from examples

--- docs/examples/BasicMulti.js
+++ docs/examples/BasicMulti.js
@@ -8,14 +8,7 @@ export default () => (
     defaultValue={[colourOptions[2], colourOptions[3]]}
     isMulti
     name="colors"
-    onValueClick={onValueClick}
     options={colourOptions}
     className="basic-multi-select"
   />
 );
-
-
-const onValueClick = (value, actionMeta) => {
-  console.info(value);
-  console.info(actionMeta);
-};
