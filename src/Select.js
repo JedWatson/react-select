732fb90cc046d724675c911ace4d0b63c150ccc7
Simeon Cheeseman <simeon@kkvesper.jp>
2018-06-08 19:22:11 +0900

    Change isOptionDisabled to have the same inputs as isOptionSelected
    As we pass the selected values to isOptionSelected, it makes sense to pass them to isOptionDisabled as well. Allows for much more dynamic select lists.

--- src/Select.js
+++ src/Select.js
@@ -118,7 +118,7 @@ export type Props = {
   /* Is the select in a state of loading (async) */
   isLoading: boolean,
   /* Override the built-in logic to detect whether an option is disabled */
-  isOptionDisabled: typeof isOptionDisabled | false,
+  isOptionDisabled: (OptionType, OptionsType) => boolean | false,
   /* Override the built-in logic to detect whether an option is selected */
   isOptionSelected?: (OptionType, OptionsType) => boolean,
   /* Support multiple selected options */
@@ -655,9 +655,9 @@ export default class Select extends Component<Props, State> {
 
     return isClearable;
   }
-  isOptionDisabled(option: OptionType): boolean {
+  isOptionDisabled(option: OptionType, selectValue: OptionsType): boolean {
     return typeof this.props.isOptionDisabled === 'function'
-      ? this.props.isOptionDisabled(option)
+      ? this.props.isOptionDisabled(option, selectValue)
       : false;
   }
   isOptionSelected(option: OptionType, selectValue: OptionsType): boolean {
@@ -988,7 +988,7 @@ export default class Select extends Component<Props, State> {
     const { hideSelectedOptions, isMulti, inputValue = '', options } = props;
 
     const toOption = (option, id) => {
-      const isDisabled = this.isOptionDisabled(option);
+      const isDisabled = this.isOptionDisabled(option, selectValue);
       const isSelected = this.isOptionSelected(option, selectValue);
       const label = this.getOptionLabel(option);
       const value = this.getOptionValue(option);
