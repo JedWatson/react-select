45a46409025f393c2da191f5cd6958ae3c3a8675
Gregory Shlyapkin <shlyapkin.g@gmail.com>
2018-07-04 09:23:17 +0700

    Fix #1620 (#2704)
    Removes duplicate className attribute on div and input

--- src/Select.js
+++ src/Select.js
@@ -879,7 +879,6 @@ class Select extends React.Component {
 			'aria-label': this.props['aria-label'],
 			'aria-labelledby': this.props['aria-labelledby'],
 			'aria-owns': ariaOwns,
-			className: className,
 			onBlur: this.handleInputBlur,
 			onChange: this.handleInputChange,
 			onFocus: this.handleInputFocus,
@@ -923,7 +922,7 @@ class Select extends React.Component {
 
 		if (this.props.autosize) {
 			return (
-				<AutosizeInput id={this.props.id} {...inputProps} minWidth="5" />
+				<AutosizeInput id={this.props.id} {...inputProps} className={className} minWidth="5" />
 			);
 		}
 		return (
