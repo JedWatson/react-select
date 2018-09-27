8e39029cf47a10566e56165632bc7e803128c6eb
Andreas Lind <andreas.lind@peakon.com>
2018-09-27 16:53:39 +0200

    Add tests

---
+++ packages/react-select/src/__tests__/filters.test.js
@@ -0,0 +1,25 @@
+import { createFilter } from '../filters';
+
+test('matchFrom > start-word > to find a match at the start of the string', () => {
+  const filter = createFilter({
+    matchFrom: 'start-word',
+    stringify: String,
+  });
+  expect(filter('bar foo', 'bar')).toBe(true);
+});
+
+test('matchFrom > start-word > to find a match at the start of a word further into the string', () => {
+  const filter = createFilter({
+    matchFrom: 'start-word',
+    stringify: String,
+  });
+  expect(filter('foo bar', 'bar')).toBe(true);
+});
+
+test('matchFrom > start-word > to ignore a match within a word', () => {
+  const filter = createFilter({
+    matchFrom: 'start-word',
+    stringify: String,
+  });
+  expect(filter('foo bar', 'oo')).toBe(false);
+});
