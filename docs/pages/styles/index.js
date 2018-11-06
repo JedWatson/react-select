1884ee38b908aad0432639b0d5252f239988ea46
mwood23 <marcuswood230@gmail.com>
2018-11-06 15:39:02 -0500

    Add menu portal to style keys

--- docs/pages/styles/index.js
+++ docs/pages/styles/index.js
@@ -57,6 +57,7 @@ export default function Styles() {
     - \`loadingMessage\`
     - \`menu\`
     - \`menuList\`
+    - \`menuPortal\`
     - \`multiValue\`
     - \`multiValueLabel\`
     - \`multiValueRemove\`
