852af512bb719c19428871bfe527a05794398033
Andreev Sergey <DragorWW@gmail.com>
2019-01-21 19:44:48 +0700

    rollup config: replace rollup-plugin-uglify on rollup-plugin-terser

--- rollup.config.js
+++ rollup.config.js
@@ -3,8 +3,7 @@
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
-import { uglify } from 'rollup-plugin-uglify';
-import { minify } from 'uglify-es';
+import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
 
 const name = 'Select';
@@ -68,6 +67,6 @@ export default [
       globals,
     },
     external,
-    plugins: [babel(babelOptions()), resolve(), commonjs(), uglify({}, minify)],
+    plugins: [babel(babelOptions()), resolve(), commonjs(), terser()],
   },
 ];
