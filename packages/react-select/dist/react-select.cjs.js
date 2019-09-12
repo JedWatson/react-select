ba58c6822eef02ae29ecf9e02cc968dd725fcf1a
Jack Spiva <johnspiva@gmail.com>
2019-09-12 16:50:16 -0500

    Remove dist

--- packages/react-select/dist/react-select.cjs.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-if (process.env.NODE_ENV === "production") {
-  module.exports = require("./react-select.cjs.prod.js");
-} else {
-  module.exports = require("./react-select.cjs.dev.js");
-}
