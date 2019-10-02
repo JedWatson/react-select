1cd7a01f7c8821561bddb389b5d50e1f4285c644
Jed Watson <jed.watson@me.com>
2019-10-02 17:05:21 +1000

    Improving sortable example behaviour

--- docs/examples/MultiSelectSort.js
+++ docs/examples/MultiSelectSort.js
@@ -10,7 +10,18 @@ function arrayMove(array, from, to) {
   return array;
 }
 
-const SortableMultiValue = SortableElement(components.MultiValue);
+const SortableMultiValue = SortableElement(props => {
+  // this prevents the menu from being opened/closed when the user clicks
+  // on a value to begin dragging it. ideally, detecting a click (instead of
+  // a drag) would still focus the control and toggle the menu, but that
+  // requires some magic with refs that are out of scope for this example
+  const onMouseDown = e => {
+    e.preventDefault();
+    e.stopPropagation();
+  };
+  const innerProps = { onMouseDown };
+  return <components.MultiValue {...props} innerProps={innerProps} />;
+});
 const SortableSelect = SortableContainer(Select);
 
 export default function MultiSelectSort() {
@@ -33,7 +44,7 @@ export default function MultiSelectSort() {
       axis="xy"
       onSortEnd={onSortEnd}
       distance={4}
-      // small fix for https://github.com/clauderic/react-sortable-hoc/pull/352 :
+      // small fix for https://github.com/clauderic/react-sortable-hoc/pull/352:
       getHelperDimensions={({ node }) => node.getBoundingClientRect()}
       // react-select props:
       isMulti
