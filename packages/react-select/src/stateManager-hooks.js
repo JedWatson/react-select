0434632a2cb774a878392c8808e05eec3e4c8c4b
mitchellhamilton <mitchell@hamil.town>
2019-10-23 10:47:56 +1000

    Fix the thing

--- packages/react-select/src/stateManager-hooks.js
+++ packages/react-select/src/stateManager-hooks.js
@@ -6,82 +6,90 @@ export const defaultProps = {
   defaultValue: null,
 };
 
-const manageState = SelectComponent => forwardRef({
-  value: valueProp,
-  onChange: onChangeProp,
-  defaultValue = null,
-  inputValue: inputValueProp,
-  onInputChange: onInputChangeProp,
-  defaultInputValue = '',
-  menuIsOpen: menuIsOpenProp,
-  onMenuOpen: onMenuOpenProp,
-  onMenuClose: onMenuCloseProp,
-  defaultMenuIsOpen = false,
-  ...props
-}, ref) => {
-  const [valueState, setValue] = useState(defaultValue);
-  const [inputValueState, setInputValue] = useState(defaultInputValue);
-  const [menuIsOpenState, setMenuIsOpen] = useState(defaultMenuIsOpen);
+const manageState = SelectComponent =>
+  forwardRef(
+    (
+      {
+        value: valueProp,
+        onChange: onChangeProp,
+        defaultValue = null,
+        inputValue: inputValueProp,
+        onInputChange: onInputChangeProp,
+        defaultInputValue = '',
+        menuIsOpen: menuIsOpenProp,
+        onMenuOpen: onMenuOpenProp,
+        onMenuClose: onMenuCloseProp,
+        defaultMenuIsOpen = false,
+        ...props
+      },
+      ref
+    ) => {
+      const [valueState, setValue] = useState(defaultValue);
+      const [inputValueState, setInputValue] = useState(defaultInputValue);
+      const [menuIsOpenState, setMenuIsOpen] = useState(defaultMenuIsOpen);
 
-  const value = valueProp !== undefined ? valueProp : valueState;
-  const inputValue =
-    inputValueProp !== undefined ? inputValueProp : inputValueState;
-  const menuIsOpen =
-    menuIsOpenProp !== undefined ? menuIsOpenProp : menuIsOpenState;
+      const value = valueProp !== undefined ? valueProp : valueState;
+      const inputValue =
+        inputValueProp !== undefined ? inputValueProp : inputValueState;
+      const menuIsOpen =
+        menuIsOpenProp !== undefined ? menuIsOpenProp : menuIsOpenState;
 
-  const onChange = useCallback(
-    (newValue, actionMeta) => {
-      if (typeof onChangeProp === 'function') {
-        onChangeProp(newValue, actionMeta);
-      }
-      setValue(newValue);
-    },
-    [onChangeProp]
-  );
-
-  const onInputChange = useCallback(
-    (newInputValue, actionMeta) => {
-      // TODO: for backwards compatibility, we allow the prop to return a new
-      // value; remove this in the next major version (breaking change)
-      const modifiedInputValue =
-        typeof onInputChangeProp === 'function'
-          ? onInputChangeProp(inputValue, actionMeta)
-          : undefined;
-      setInputValue(
-        modifiedInputValue !== undefined ? modifiedInputValue : newInputValue,
-        actionMeta
+      const onChange = useCallback(
+        (newValue, actionMeta) => {
+          if (typeof onChangeProp === 'function') {
+            onChangeProp(newValue, actionMeta);
+          }
+          setValue(newValue);
+        },
+        [onChangeProp]
       );
-    },
-    [onInputChangeProp]
-  );
 
-  const onMenuOpen = useCallback(() => {
-    if (typeof onMenuOpenProp === 'function') {
-      onMenuOpenProp();
+      const onInputChange = useCallback(
+        (newInputValue, actionMeta) => {
+          // TODO: for backwards compatibility, we allow the prop to return a new
+          // value; remove this in the next major version (breaking change)
+          const modifiedInputValue =
+            typeof onInputChangeProp === 'function'
+              ? onInputChangeProp(inputValue, actionMeta)
+              : undefined;
+          setInputValue(
+            modifiedInputValue !== undefined
+              ? modifiedInputValue
+              : newInputValue,
+            actionMeta
+          );
+        },
+        [onInputChangeProp]
+      );
+
+      const onMenuOpen = useCallback(() => {
+        if (typeof onMenuOpenProp === 'function') {
+          onMenuOpenProp();
+        }
+        setMenuIsOpen(true);
+      }, [onMenuOpenProp]);
+
+      const onMenuClose = useCallback(() => {
+        if (typeof onMenuCloseProp === 'function') {
+          onMenuCloseProp();
+        }
+        setMenuIsOpen(false);
+      }, [onMenuCloseProp]);
+
+      return (
+        <SelectComponent
+          {...props}
+          ref={ref}
+          value={value}
+          onChange={onChange}
+          inputValue={inputValue}
+          onInputChange={onInputChange}
+          menuIsOpen={menuIsOpen}
+          onMenuOpen={onMenuOpen}
+          onMenuClose={onMenuClose}
+        />
+      );
     }
-    setMenuIsOpen(true);
-  }, [onMenuOpenProp]);
-
-  const onMenuClose = useCallback(() => {
-    if (typeof onMenuCloseProp === 'function') {
-      onMenuCloseProp();
-    }
-    setMenuIsOpen(false);
-  }, [onMenuCloseProp]);
-
-  return (
-    <SelectComponent
-      {...props}
-      ref={ref}
-      value={value}
-      onChange={onChange}
-      inputValue={inputValue}
-      onInputChange={onInputChange}
-      menuIsOpen={menuIsOpen}
-      onMenuOpen={onMenuOpen}
-      onMenuClose={onMenuClose}
-    />
   );
-};
 
 export default manageState;
