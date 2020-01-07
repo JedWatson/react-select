4750e35b138a95703c882547c8a82a1ea0e5f0bc
Loris Aranda <laranda@student.42.fr>
2020-01-07 17:25:10 +0100

    Fixes #3335 - remove the call to onMenuOpen within handleInputChange in Select.js

--- packages/react-select/src/Select.js
+++ packages/react-select/src/Select.js
@@ -1089,7 +1089,6 @@ export default class Select extends Component<Props, State> {
     const inputValue = event.currentTarget.value;
     this.inputIsHiddenAfterUpdate = false;
     this.onInputChange(inputValue, { action: 'input-change' });
-    this.onMenuOpen();
   };
   onInputFocus = (event: SyntheticFocusEvent<HTMLInputElement>) => {
     const { isSearchable, isMulti } = this.props;
