02651d3f22073811665bffaf17f0df20f2495f76
mitchellhamilton <mitchell@hamil.town>
2020-03-18 12:06:18 +1100

    Fix some things

--- enzymeAdapter.setup.js
+++ enzymeAdapter.setup.js
@@ -1,4 +1,5 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import '@testing-library/jest-dom';
 
 Enzyme.configure({ adapter: new Adapter() });
--- packages/react-select/src/__tests__/StateManaged.test.js
+++ packages/react-select/src/__tests__/StateManaged.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import '@testing-library/jest-dom';
 
 import cases from 'jest-in-case';
 
