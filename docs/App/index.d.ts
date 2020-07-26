1af590aef11b94d69dbb28c3f159cc571afd2beb
Nathan Bierema <nbierema@gmail.com>
2020-07-25 20:46:21 -0400

    Cleanup

---
+++ docs/App/index.d.ts
@@ -0,0 +1,5 @@
+import { Component } from 'react';
+
+export default class App extends Component {
+
+}
---
+++ docs/index.js
@@ -0,0 +1,10 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+require("@babel/polyfill");
+const react_1 = __importDefault(require("react"));
+const react_dom_1 = __importDefault(require("react-dom"));
+const App_1 = __importDefault(require("./App"));
+react_dom_1.default.render(react_1.default.createElement(App_1.default, null), document.getElementById('root'));
