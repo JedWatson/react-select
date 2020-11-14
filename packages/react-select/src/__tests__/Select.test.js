1cc4af814d7f121ff81d918c5bd0420f7275c6ff
Stéphane Monnot <monnot.stephane@gmail.com>
2020-11-14 21:07:11 +0900

    Fix lint error

--- packages/react-select/src/__tests__/Select.test.js
+++ packages/react-select/src/__tests__/Select.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent, waitFor } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import cases from 'jest-in-case';
 
