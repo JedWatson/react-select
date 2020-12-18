fcdeb2b4c5a20d7315cce365c8be794bcc614fb1
Nathan Bierema <nbierema@gmail.com>
2020-12-17 22:04:13 -0500

    Pass [] instead of null in onChange when no values on multi-select

--- packages/react-select/src/Select.js
+++ packages/react-select/src/Select.js
@@ -682,12 +682,13 @@ export default class Select extends Component<Props, State> {
     }
   };
   removeValue = (removedValue: OptionType) => {
+    const { isMulti } = this.props;
     const { selectValue } = this.state;
     const candidate = this.getOptionValue(removedValue);
     const newValue = selectValue.filter(
       i => this.getOptionValue(i) !== candidate
     );
-    this.onChange(newValue.length ? newValue : null, {
+    this.onChange(isMulti ? newValue : null, {
       action: 'remove-value',
       removedValue,
     });
@@ -704,6 +705,7 @@ export default class Select extends Component<Props, State> {
     this.onChange(isMulti ? [] : null, { action: 'clear' });
   };
   popValue = () => {
+    const { isMulti } = this.props;
     const { selectValue } = this.state;
     const lastSelectedValue = selectValue[selectValue.length - 1];
     const newValue = selectValue.slice(0, selectValue.length - 1);
@@ -713,7 +715,7 @@ export default class Select extends Component<Props, State> {
         value: lastSelectedValue ? this.getOptionLabel(lastSelectedValue) : '',
       },
     });
-    this.onChange(newValue.length ? newValue : null, {
+    this.onChange(isMulti ? newValue : null, {
       action: 'pop-value',
       removedValue: lastSelectedValue,
     });
