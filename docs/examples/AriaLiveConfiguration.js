99710e9107f1278160e64fe349e6df62b0d6aa48
Eric Bonow <ebonow@hotmail.com>
2021-01-20 19:15:26 -0800

    Add accessibility section and example to documentation

---
+++ docs/examples/AriaLiveConfiguration.js
@@ -0,0 +1,29 @@
+// @flow
+import React from 'react';
+
+import Select from 'react-select';
+import { colourOptions } from '../data';
+
+
+export default function AriaLiveConfiguration() {
+  return (
+    <form>
+      <label id="aria-live-select-label" htmlFor="select-aria-live-example">
+        Test form label
+      </label>
+
+      <Select
+        aria-labelledby="select-label"
+        ariaLiveMessages={{
+          focusOption:
+            ({ focusedOption, getOptionLabel }) => {
+              return `custom aria option focus message: ${getOptionLabel(focusedOption)}`;
+            }
+        }}
+        id="select-aria-live-example"
+        name="color"
+        options={colourOptions}
+      />
+    </form>
+  );
+}
