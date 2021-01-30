0e451c5bb2b937195cea7b457d03f9e2b00b9d1f
Nathan Bierema <nbierema@gmail.com>
2021-01-30 08:42:00 -0500

    Remove skipLibCheck

--- packages/react-select/src/index.d.ts
+++ packages/react-select/src/index.d.ts
@@ -8,26 +8,34 @@
 //                 Kyle Holmberg <https://github.com/kylemh>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-import { StateManager } from './src/stateManager';
+import { StateManager } from './stateManager';
 
 export default StateManager;
 
-export * from './src/types';
-export { createFilter } from './src/filters';
-export { mergeStyles, Styles, StylesConfig } from './src/styles';
-export { defaultTheme } from './src/theme';
+export * from './types';
+export { createFilter } from './filters';
+export { mergeStyles, Styles, StylesConfig } from './styles';
+export { defaultTheme } from './theme';
 
-export { NonceProvider } from './src/NonceProvider';
-export { Props, FormatOptionLabelMeta, NamedProps } from './src/Select';
+export { NonceProvider } from './NonceProvider';
+export { Props, FormatOptionLabelMeta, NamedProps } from './Select';
 
-export { components, SelectComponentsConfig, IndicatorComponentType } from './src/components';
-export { ValueContainerProps, ContainerProps, IndicatorContainerProps } from './src/components/containers';
-export { IndicatorProps } from './src/components/indicators';
-export { ControlProps } from './src/components/Control';
-export { GroupProps } from './src/components/Group';
-export { InputProps } from './src/components/Input';
-export { MenuProps, MenuListComponentProps } from './src/components/Menu';
-export { MultiValueProps } from './src/components/MultiValue';
-export { OptionProps } from './src/components/Option';
-export { PlaceholderProps } from './src/components/Placeholder';
-export { SingleValueProps } from './src/components/SingleValue';
+export {
+  components,
+  SelectComponentsConfig,
+  IndicatorComponentType,
+} from './components';
+export {
+  ValueContainerProps,
+  ContainerProps,
+  IndicatorContainerProps,
+} from './components/containers';
+export { IndicatorProps } from './components/indicators';
+export { ControlProps } from './components/Control';
+export { GroupProps } from './components/Group';
+export { InputProps } from './components/Input';
+export { MenuProps, MenuListComponentProps } from './components/Menu';
+export { MultiValueProps } from './components/MultiValue';
+export { OptionProps } from './components/Option';
+export { PlaceholderProps } from './components/Placeholder';
+export { SingleValueProps } from './components/SingleValue';
