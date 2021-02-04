a8724c40aad6f25be03cbfc49098073e68e0b46f
Nathan Bierema <nbierema@gmail.com>
2021-02-04 13:32:31 -0500

    Add test

--- cypress/integration/single-select.spec.js
+++ cypress/integration/single-select.spec.js
@@ -171,6 +171,16 @@ describe('Single Select', () => {
           .get(selector.focusedOption)
           .should('exist');
       });
+
+      it(`Should focus next option on down arrow key press after filtering: ${viewport}`, () => {
+        cy.get(selector.singleGroupedSelect)
+          .click()
+          .find('input')
+          .type('o', { force: true })
+          .type('{downarrow}', { force: true })
+          .get(selector.focusedOption)
+          .should('exist');
+      });
     });
 
     context(`Clearable in view: ${viewport}`, () => {
