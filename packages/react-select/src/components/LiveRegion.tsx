072ae7601cb090a1b47f11cb7ce2a51b22d91680
Matt Charlton <mcharlton@atlassian.com>
2021-06-29 11:29:04 +1000

    remove console.log

--- packages/react-select/src/components/LiveRegion.tsx
+++ packages/react-select/src/components/LiveRegion.tsx
@@ -111,8 +111,6 @@ const LiveRegion = <
         ...ariaSelection,
       };
 
-      console.log('onChangeProps', onChangeProps);
-
       message = messages.onChange(onChangeProps);
     }
     return message;
