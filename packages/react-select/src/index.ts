ad068899607696a46affe6f8165c4f5a10eceacb
ps.sergeev <ps.sergeev@aisgorod.ru>
2021-10-29 16:43:29 +0400

    export hooks

--- packages/react-select/src/index.ts
+++ packages/react-select/src/index.ts
@@ -1,5 +1,8 @@
 import Select from './Select';
 import { GroupBase } from './types';
+import useAsync from './useAsync';
+import useCreatable from './useCreatable';
+import useStateManager from './useStateManager';
 
 export { default } from './stateManager';
 export { default as NonceProvider } from './NonceProvider';
@@ -13,6 +16,7 @@ export type SelectInstance<
   Group extends GroupBase<Option> = GroupBase<Option>
 > = Select<Option, IsMulti, Group>;
 export type { StateManagerProps as Props } from './useStateManager';
+export { useAsync, useCreatable, useStateManager };
 
 export type { SelectComponentsConfig } from './components';
 export type {
