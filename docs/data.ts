6a0215a8fe65a51711a91acc8794ea95fc16651d
Andy Lavor <andersonlavor@gmail.com>
2022-06-01 20:33:54 -0500

    working sortable component

--- docs/data.ts
+++ docs/data.ts
@@ -1,4 +1,5 @@
 export interface ColourOption {
+  readonly id: number;
   readonly value: string;
   readonly label: string;
   readonly color: string;
@@ -6,17 +7,17 @@ export interface ColourOption {
   readonly isDisabled?: boolean;
 }
 
-export const colourOptions: readonly ColourOption[] = [
-  { value: 'ocean', label: 'Ocean', color: '#00B8D9', isFixed: true },
-  { value: 'blue', label: 'Blue', color: '#0052CC', isDisabled: true },
-  { value: 'purple', label: 'Purple', color: '#5243AA' },
-  { value: 'red', label: 'Red', color: '#FF5630', isFixed: true },
-  { value: 'orange', label: 'Orange', color: '#FF8B00' },
-  { value: 'yellow', label: 'Yellow', color: '#FFC400' },
-  { value: 'green', label: 'Green', color: '#36B37E' },
-  { value: 'forest', label: 'Forest', color: '#00875A' },
-  { value: 'slate', label: 'Slate', color: '#253858' },
-  { value: 'silver', label: 'Silver', color: '#666666' },
+export const colourOptions: ColourOption[] = [
+  {id: 1, value: 'ocean', label: 'Ocean', color: '#00B8D9', isFixed: true },
+  {id: 2, value: 'blue', label: 'Blue', color: '#0052CC', isDisabled: true },
+  {id: 3, value: 'purple', label: 'Purple', color: '#5243AA' },
+  {id: 4, value: 'red', label: 'Red', color: '#FF5630', isFixed: true },
+  {id: 5, value: 'orange', label: 'Orange', color: '#FF8B00' },
+  {id: 6, value: 'yellow', label: 'Yellow', color: '#FFC400' },
+  {id: 7, value: 'green', label: 'Green', color: '#36B37E' },
+  {id: 8, value: 'forest', label: 'Forest', color: '#00875A' },
+  {id: 9, value: 'slate', label: 'Slate', color: '#253858' },
+  {id: 10, value: 'silver', label: 'Silver', color: '#666666' },
 ];
 
 export interface FlavourOption {
--- docs/examples/MultiSelectSort.tsx
+++ docs/examples/MultiSelectSort.tsx
@@ -1,93 +1,135 @@
-import React from 'react';
-
+import React  from 'react';
+import { CSS } from '@dnd-kit/utilities';
 import Select, {
-  // components,
-  // MultiValueGenericProps,
-  // MultiValueProps,
+  components,
+  MultiValueGenericProps, MultiValueProps, MultiValueRemoveProps,
   OnChangeValue,
-  // Props,
 } from 'react-select';
-// import {
-//   SortableContainer,
-//   SortableContainerProps,
-//   SortableElement,
-//   SortEndHandler,
-//   SortableHandle,
-// } from 'react-sortable-hoc';
 import { ColourOption, colourOptions } from '../data';
+import Tooltip from '@atlaskit/tooltip';
+import { DndContext, DragEndEvent, useDraggable, useDroppable } from '@dnd-kit/core';
+import { restrictToParentElement } from '@dnd-kit/modifiers';
+import { arrayMove, horizontalListSortingStrategy, SortableContext, useSortable } from '@dnd-kit/sortable';
 
-// function arrayMove<T>(array: readonly T[], from: number, to: number) {
-//   const slicedArray = array.slice();
-//   slicedArray.splice(
-//     to < 0 ? array.length + to : to,
-//     0,
-//     slicedArray.splice(from, 1)[0]
-//   );
-//   return slicedArray;
-// }
+export const MultiValue = (props: MultiValueProps<ColourOption>) => {
+    // this prevents the menu from being opened/closed when the user clicks
+    // on a value to begin dragging it. ideally, detecting a click (instead of
+    // a drag) would still focus the control and toggle the menu, but that
+    // requires some magic with refs that are out of scope for this example
+    // const onMouseDown: MouseEventHandler<HTMLDivElement> = (e) => {
+      // e.preventDefault();
+      // e.stopPropagation();
+    // };
+    const innerProps = { ...props.innerProps, };
+    const { attributes, listeners, setNodeRef, transform, transition } = useSortable({
+    id: props.data.id,
+  });
 
-// const SortableMultiValue = SortableElement(
-//   (props: MultiValueProps<ColourOption>) => {
-//     // this prevents the menu from being opened/closed when the user clicks
-//     // on a value to begin dragging it. ideally, detecting a click (instead of
-//     // a drag) would still focus the control and toggle the menu, but that
-//     // requires some magic with refs that are out of scope for this example
-//     const onMouseDown: MouseEventHandler<HTMLDivElement> = (e) => {
-//       e.preventDefault();
-//       e.stopPropagation();
-//     };
-//     const innerProps = { ...props.innerProps, onMouseDown };
-//     return <components.MultiValue {...props} innerProps={innerProps} />;
-//   }
-// );
-
-// const SortableMultiValueLabel = SortableHandle(
-//   (props: MultiValueGenericProps) => <components.MultiValueLabel {...props} />
-// );
-
-// const SortableSelect = SortableContainer(Select) as React.ComponentClass<
-//   Props<ColourOption, true> & SortableContainerProps
-// >;
-
-export default function MultiSelectSort() {
-  const [selected, setSelected] = React.useState<readonly ColourOption[]>([
-    colourOptions[4],
-    colourOptions[5],
-  ]);
-
-  const onChange = (selectedOptions: OnChangeValue<ColourOption, true>) =>
-    setSelected(selectedOptions);
-
-  // const onSortEnd: SortEndHandler = ({ oldIndex, newIndex }) => {
-  //   const newValue = arrayMove(selected, oldIndex, newIndex);
-  //   setSelected(newValue);
-  //   console.log(
-  //     'Values sorted:',
-  //     newValue.map((i) => i.value)
-  //   );
-  // };
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
 
   return (
-    // <SortableSelect
-    <Select
-      // useDragHandle
-      // react-sortable-hoc props:
-      // axis="xy"
-      // onSortEnd={onSortEnd}
-      // distance={4}
-      // small fix for https://github.com/clauderic/react-sortable-hoc/pull/352:
-      // getHelperDimensions={({ node }) => node.getBoundingClientRect()}
-      // react-select props:
-      isMulti
-      options={colourOptions}
-      value={selected}
-      onChange={onChange}
-      components={{
-        // @ts-ignore We're failing to provide a required index prop to SortableElement
-        MultiValue: SortableMultiValue,
-        // MultiValueLabel: SortableMultiValueLabel,
-      }}
-      closeMenuOnSelect={false}
-    />
+            <div
+              style={style}
+              ref={setNodeRef}
+              {...attributes}
+              {...listeners}
+            >
+              <components.MultiValue  {...props} innerProps={innerProps} />
+            </div>);
+  };
+
+
+export const MultiValueContainer = (props: MultiValueGenericProps<ColourOption>) => {
+  const { isOver, setNodeRef } = useDroppable({
+    id: 'droppable',
+  });
+
+  const style = {
+    color: isOver ? 'green' : undefined,
+  };
+  return (
+    <Tooltip content={'Customise your multi-value container!'}>
+      <div style={style} ref={setNodeRef}>
+        <components.MultiValueContainer {...props} />
+      </div>
+    </Tooltip>
+  );
+};
+
+export const MultiValueLabel = (props: MultiValueGenericProps<ColourOption>) => {
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
+    id: 'draggable',
+  });
+
+  const style = transform ? {
+    transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
+  } : undefined;
+
+  return (
+    <Tooltip content={'Customise your multi-value label component111!'}>
+      <div style={style} ref={setNodeRef} {...listeners} {...attributes}>
+      <components.MultiValueLabel {...props} />
+      </div>
+    </Tooltip>
+  );
+};
+
+export const MultiValueRemove = (props: MultiValueRemoveProps<ColourOption>) => {
+  // console.log('MultiValueRemove', props.data.id);
+  return (
+    <div onClick={()=> console.log('clicked on ->', props.data.id)} style={{zIndex: 99999}}>
+      <components.MultiValueRemove {...props}  />
+    </div>
+  );
+};
+
+
+export default function MultiSelectSort() {
+  const [selected, setSelected] = React.useState<ColourOption[]>([...[
+    colourOptions[4],
+    colourOptions[5],
+  ]]);
+
+  const onChange = (selectedOptions: OnChangeValue<ColourOption, true>) =>
+    setSelected([...selectedOptions]);
+
+  const onSortEnd = (event: DragEndEvent) => {
+    const { active, over } = event;
+
+    if (!active || !over) return;
+
+    setSelected((items) => {
+      const oldIndex = items.findIndex((item) => item.id === active.id);
+      const newIndex = items.findIndex((item) => item.id === over.id);
+      return arrayMove(items, oldIndex, newIndex);
+    });
+
+  };
+
+  // const { setNodeRef } = useDraggable({})
+
+  return (
+    <DndContext modifiers={[restrictToParentElement]} onDragEnd={onSortEnd}>
+      <SortableContext items={selected} strategy={horizontalListSortingStrategy}>
+        <Select
+          useDragHandle
+          distance={4}
+          isMulti
+          options={colourOptions}
+          value={selected}
+          onChange={onChange}
+          components={{
+            // @ts-ignore We're failing to provide a required index prop to SortableElement
+            MultiValue,
+            MultiValueContainer,
+            MultiValueRemove
+          }}
+          closeMenuOnSelect={false}
+        />
+      </SortableContext>
+    </DndContext>
   );
 }
