5e52def4c9c50f34da8fb0b1e29eb1635a3feca0
Patrick Connolly <patrick.c.connolly@gmail.com>
2022-07-18 11:49:52 -0400

    Ignored no-unused-vars, as giving false positives for exported interface.

--- docs/examples/CustomSelectProps.tsx
+++ docs/examples/CustomSelectProps.tsx
@@ -11,7 +11,9 @@ import { ColourOption, colourOptions } from '../data';
 declare module 'react-select/dist/declarations/src/Select' {
   export interface Props<
     Option,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     IsMulti extends boolean,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     Group extends GroupBase<Option>
   > {
     emoji: string;
