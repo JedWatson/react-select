e2a156f2e63349b473b9f51206e44f7da9538286
Luke Bennett <hello@lukebennett.com.au>
2022-11-01 13:20:41 +1100

    Update Popout

--- storybook/stories/Popout.stories.tsx
+++ storybook/stories/Popout.stories.tsx
@@ -4,7 +4,7 @@ import type { ComponentMeta } from '@storybook/react';
 import * as React from 'react';
 import Select, { StylesConfig } from 'react-select';
 
-import { Button } from '../components';
+import { Button, ChevronDown, Svg } from '../components';
 import { StateOption, stateOptions } from '../data';
 
 export default {
@@ -100,19 +100,6 @@ function Dropdown({
   );
 }
 
-function Svg(props: JSX.IntrinsicElements['svg']) {
-  return (
-    <svg
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      focusable="false"
-      role="presentation"
-      {...props}
-    />
-  );
-}
-
 function DropdownIndicator() {
   return (
     <div className="h-6 w-8 text-gray-300">
@@ -124,17 +111,5 @@ function DropdownIndicator() {
         />
       </Svg>
     </div>
-  );
-}
-
-function ChevronDown() {
-  return (
-    <Svg className="-mr-1.5">
-      <path
-        d="M8.292 10.293a1.009 1.009 0 0 0 0 1.419l2.939 2.965c.218.215.5.322.779.322s.556-.107.769-.322l2.93-2.955a1.01 1.01 0 0 0 0-1.419.987.987 0 0 0-1.406 0l-2.298 2.317-2.307-2.327a.99.99 0 0 0-1.406 0z"
-        fill="currentColor"
-        fillRule="evenodd"
-      />
-    </Svg>
   );
 }
