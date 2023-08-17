e33a9f28f67fc3900a24b0f8ed8628f4e7eb631a
Milad Karandish <80484107+MiladKarandish@users.noreply.github.com>
2023-08-17 12:04:10 +0330

    In DummyInput.tsx, Input CSS hack replaced with improved and efficient code.

--- packages/react-select/src/internal/DummyInput.tsx
+++ packages/react-select/src/internal/DummyInput.tsx
@@ -25,26 +25,10 @@ export default function DummyInput({
       {...filteredProps}
       css={{
         label: 'dummyInput',
-        // get rid of any default styles
-        background: 0,
-        border: 0,
-        // important! this hides the flashing cursor
-        caretColor: 'transparent',
-        fontSize: 'inherit',
-        gridArea: '1 / 1 / 2 / 3',
-        outline: 0,
-        padding: 0,
-        // important! without `width` browsers won't allow focus
-        width: 1,
-
-        // remove cursor on desktop
-        color: 'transparent',
-
-        // remove cursor on mobile whilst maintaining "scroll into view" behaviour
-        left: -100,
-        opacity: 0,
-        position: 'relative',
-        transform: 'scale(.01)',
+        width: '1px',
+        position: 'absolute',
+        pointerEvents: 'none',
+        opacity: '0',
       }}
     />
   );
