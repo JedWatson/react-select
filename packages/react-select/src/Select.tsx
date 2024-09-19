dd740cedb29c810a89da4445d4864cd7e63d3aaf
Leon Aves <leonaves1@gmail.com>
2024-09-19 15:38:01 +0200

    Fix pop-value being sent for undefined values. (#5960)
    * Fix pop-value being sent for undefined values.
    
    * Fix unit tests.
    
    * Fix linting.
    
    * Add changeset.

--- packages/react-select/src/Select.tsx
+++ packages/react-select/src/Select.tsx
@@ -1096,10 +1096,12 @@ export default class Select<
       newValueArray[0] || null
     );
 
-    this.onChange(newValue, {
-      action: 'pop-value',
-      removedValue: lastSelectedValue,
-    });
+    if (lastSelectedValue) {
+      this.onChange(newValue, {
+        action: 'pop-value',
+        removedValue: lastSelectedValue,
+      });
+    }
   };
 
   // ==============================
